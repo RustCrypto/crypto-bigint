500bc97aae4b878875ccd143b5b90c8b31b62a3d
Tony Arcieri <bascule@gmail.com>
2021-11-14 07:29:55 -0700

    Remove deprecated `LIMB_BYTES` constant (#43)
    The replacement is: `Limb::BYTE_SIZE`

--- src/lib.rs
+++ src/lib.rs
@@ -81,13 +81,3 @@ pub use rlp;
 
 #[cfg(feature = "zeroize")]
 pub use zeroize;
-
-/// Number of bytes in a [`Limb`].
-#[cfg(target_pointer_width = "32")]
-#[deprecated(since = "0.2.2", note = "use `Limb::BYTE_SIZE` instead")]
-pub const LIMB_BYTES: usize = 4;
-
-/// Number of bytes in a [`Limb`].
-#[cfg(target_pointer_width = "64")]
-#[deprecated(since = "0.2.2", note = "use `Limb::BYTE_SIZE` instead")]
-pub const LIMB_BYTES: usize = 8;
