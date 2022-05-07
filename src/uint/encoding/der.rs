ef7d22da949628079fd1c0c8c8e7636b83720d60
Tony Arcieri <bascule@gmail.com>
2022-05-07 09:03:22 -0600

    Bump `der` dependency to v0.6.0-pre.4 (#78)
    Also adds a `DecodeValue` impl for `UInt`

--- src/uint/encoding/der.rs
+++ src/uint/encoding/der.rs
@@ -2,32 +2,30 @@
 
 use crate::{generic_array::GenericArray, ArrayEncoding, UInt};
 use ::der::{
-    asn1::{Any, UIntBytes},
-    EncodeValue, Encoder, FixedTag, Length, Tag,
+    asn1::{AnyRef, UIntRef},
+    DecodeValue, EncodeValue, FixedTag, Length, Tag,
 };
 
-#[cfg(feature = "der")]
 #[cfg_attr(docsrs, doc(cfg(feature = "der")))]
-impl<'a, const LIMBS: usize> TryFrom<Any<'a>> for UInt<LIMBS>
+impl<'a, const LIMBS: usize> TryFrom<AnyRef<'a>> for UInt<LIMBS>
 where
     UInt<LIMBS>: ArrayEncoding,
 {
     type Error = der::Error;
 
-    fn try_from(any: Any<'a>) -> der::Result<UInt<LIMBS>> {
-        UIntBytes::try_from(any)?.try_into()
+    fn try_from(any: AnyRef<'a>) -> der::Result<UInt<LIMBS>> {
+        UIntRef::try_from(any)?.try_into()
     }
 }
 
-#[cfg(feature = "der")]
 #[cfg_attr(docsrs, doc(cfg(feature = "der")))]
-impl<'a, const LIMBS: usize> TryFrom<UIntBytes<'a>> for UInt<LIMBS>
+impl<'a, const LIMBS: usize> TryFrom<UIntRef<'a>> for UInt<LIMBS>
 where
     UInt<LIMBS>: ArrayEncoding,
 {
     type Error = der::Error;
 
-    fn try_from(bytes: UIntBytes<'a>) -> der::Result<UInt<LIMBS>> {
+    fn try_from(bytes: UIntRef<'a>) -> der::Result<UInt<LIMBS>> {
         let mut array = GenericArray::default();
         let offset = array.len().saturating_sub(bytes.len().try_into()?);
         array[offset..].copy_from_slice(bytes.as_bytes());
@@ -35,7 +33,16 @@ where
     }
 }
 
-#[cfg(feature = "der")]
+#[cfg_attr(docsrs, doc(cfg(feature = "der")))]
+impl<'a, const LIMBS: usize> DecodeValue<'a> for UInt<LIMBS>
+where
+    UInt<LIMBS>: ArrayEncoding,
+{
+    fn decode_value<R: der::Reader<'a>>(reader: &mut R, header: der::Header) -> der::Result<Self> {
+        UIntRef::decode_value(reader, header)?.try_into()
+    }
+}
+
 #[cfg_attr(docsrs, doc(cfg(feature = "der")))]
 impl<const LIMBS: usize> EncodeValue for UInt<LIMBS>
 where
@@ -44,16 +51,15 @@ where
     fn value_len(&self) -> der::Result<Length> {
         // TODO(tarcieri): more efficient length calculation
         let array = self.to_be_byte_array();
-        UIntBytes::new(&array)?.value_len()
+        UIntRef::new(&array)?.value_len()
     }
 
-    fn encode_value(&self, encoder: &mut Encoder<'_>) -> der::Result<()> {
+    fn encode_value(&self, encoder: &mut dyn der::Writer) -> der::Result<()> {
         let array = self.to_be_byte_array();
-        UIntBytes::new(&array)?.encode_value(encoder)
+        UIntRef::new(&array)?.encode_value(encoder)
     }
 }
 
-#[cfg(feature = "der")]
 #[cfg_attr(docsrs, doc(cfg(feature = "der")))]
 impl<const LIMBS: usize> FixedTag for UInt<LIMBS>
 where
