c112341b6230a5f131f428332c9a192f48748f67
Tony Arcieri <bascule@gmail.com>
2022-08-26 14:08:59 -0600

    dudect: add constant-time test for `UInt::reduce`
    Uses the `dudect-bencher` crate, a wrapper for the `dudect` C library:
    
    https://github.com/oreparaz/dudect
    
    Example output:
    
    ```
    running 1 bench
    bench reduce seeded with 0x2311ed8a8b8e6544
    bench reduce ... : n == +0.100M, max t = +1.98296, max tau = +0.00628, (5/tau)^2 = 634328
    
    dudect benches complete
    ```
    
    The important part here is `max t = +1.98296`. If `t` were to exceed 10,
    then the code is likely *not* constant time. Since it's well below 10,
    `reduce` appears to be OK.
    
    --
    
    Also adds a CI check for `max t`. The threshold is currently set to 100x
    which is 10x higher than dudect's recommended 10, but it's set high to
    avoid noise/false positives.
    
    We can potentially adjust it if that's either too lenient or ends up
    causing too many false positives.

---
+++ dudect/src/main.rs
@@ -0,0 +1,43 @@
+//! Fuzz tests for `crypto-bigint`.
+//!
+//! These use the `dudect_bencher` crate to check certain operations for
+//! constant-time behavior.
+
+use crypto_bigint::{Random, U128, U256};
+use dudect_bencher::{ctbench_main, BenchRng, Class, CtRunner};
+
+/// Check `UInt::reduce` for constant-time operation.
+fn reduce(runner: &mut CtRunner, mut rng: &mut BenchRng) {
+    const ITERATIONS_OUTER: usize = 10_000;
+    const ITERATIONS_INNER: usize = 10_000;
+
+    // 128-bit random example
+    let a = U128::ZERO.concat(&U128::random(&mut rng));
+
+    // 256-bit random example
+    let b = U256::random(&mut rng);
+
+    // Random modulus (256-bit)
+    let modulus = U256::random(&mut rng);
+
+    // Precomputing the inputs appears to eliminate some noise
+    let mut inputs = vec![];
+
+    for _ in 0..ITERATIONS_OUTER {
+        inputs.push((Class::Left, a));
+    }
+
+    for _ in 0..ITERATIONS_OUTER {
+        inputs.push((Class::Right, b));
+    }
+
+    for (class, input) in inputs {
+        runner.run_one(class, || {
+            for _ in 0..ITERATIONS_INNER {
+                input.reduce(&modulus);
+            }
+        })
+    }
+}
+
+ctbench_main!(reduce);
