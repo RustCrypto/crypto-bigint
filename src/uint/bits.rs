aee1ac0390d1c13dea29adb1ba48113838bb4db3
Andrew Whitehead <cywolf@gmail.com>
2022-09-20 09:29:08 -0700

    add bit accessor method
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/uint/bits.rs
+++ src/uint/bits.rs
@@ -1,6 +1,13 @@
 use crate::{Limb, UInt, Word};
 
 impl<const LIMBS: usize> UInt<LIMBS> {
+    /// Get the value of the bit at position `index`, as a 0- or 1-valued Word.
+    #[inline]
+    pub const fn bit(self, index: usize) -> Word {
+        assert!(index < LIMBS * Limb::BIT_SIZE);
+        (self.limbs[index / Limb::BIT_SIZE].0 >> (index % Limb::BIT_SIZE)) & 1
+    }
+
     /// Calculate the number of bits needed to represent this number.
     #[allow(trivial_numeric_casts)]
     pub const fn bits(self) -> usize {
@@ -18,5 +25,18 @@ impl<const LIMBS: usize> UInt<LIMBS> {
             !self.limbs[0].is_nonzero() & !Limb(i as Word).is_nonzero(),
         )
         .0 as usize
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::U64;
+
+    #[test]
+    fn bit_get_ok() {
+        let u = U64::from_be_hex("d201000000010000");
+        assert_eq!(u.bit(0), 0);
+        assert_eq!(u.bit(1), 0);
+        assert_eq!(u.bit(16), 1);
     }
 }
