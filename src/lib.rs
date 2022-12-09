RustCrypto/crypto-bigint

commit 5b2e903e5cff0823a71ea2bca019637e6fa33bf6

Implement modular arithmetic; MSRV 1.61 (#130)
Adds the following functionality:

- Shifting left and right by 1 and returning the overflow as a `Choice`
- `Neg` for `Wrapping<Uint<LIMBS>>`
- Modular multiplicative inverse
- Computing parameters for the Montgomery form (MF)
- Going to and from MF
- Modular addition (in MF)
- Modular multiplication (in MF)
- Modular exponentiation (in MF)

Cargo.toml
@@ -14,7 +14,7 @@ keywords = ["arbitrary", "crypto", "bignum", "integer", "precision"]
 readme = "README.md"
 resolver = "2"
 edition = "2021"
-rust-version = "1.57"
+rust-version = "1.61"
 
 [dependencies]
 subtle = { version = "2.4", default-features = false }

src/lib.rs
@@ -47,6 +47,15 @@
 //! pub const MODULUS_SHR1: U256 = MODULUS.shr_vartime(1);
 //! ```
 //!
+//! Note that large constant computations may accidentally trigger a the `const_eval_limit` of the compiler.
+//! The current way to deal with this problem is to either simplify this computation,
+//! or increase the compiler's limit (currently a nightly feature).
+//! One can completely remove the compiler's limit using:
+//! ```ignore
+//! #![feature(const_eval_limit)]
+//! #![const_eval_limit = "0"]
+//! ```
+//!
 //! ### Trait-based usage
 //!
 //! The [`UInt`] type itself does not implement the standard arithmetic traits
@@ -99,6 +108,10 @@
 //! let b = a.add_mod(&U256::ONE, &modulus);
 //! assert_eq!(b, U256::ZERO);
 //! ```
+//!
+//! It also supports modular arithmetic over constant moduli using `Residue`.
+//! That includes modular exponentiation and multiplicative inverses.
+//! These features are described in the [`modular`] module.
 //!
 //! ### Random number generation
 //!

src/uint.rs
@@ -25,6 +25,7 @@ mod from;
 mod inv_mod;
 mod mul;
 mod mul_mod;
+mod neg;
 mod neg_mod;
 mod resize;
 mod shl;
@@ -32,6 +33,9 @@ mod shr;
 mod sqrt;
 mod sub;
 mod sub_mod;
+
+/// Implements modular arithmetic for constant moduli.
+pub mod modular;
 
 #[cfg(feature = "generic-array")]
 mod array;

.github/workflows/crypto-bigint.yml
@@ -19,7 +19,7 @@ jobs:
     strategy:
       matrix:
         rust:
-          - 1.57.0 # MSRV
+          - 1.61.0 # MSRV
           - stable
         target:
           - thumbv7em-none-eabi
@@ -49,7 +49,7 @@ jobs:
         include:
           # 32-bit Linux
           - target: i686-unknown-linux-gnu
-            rust: 1.57.0 # MSRV
+            rust: 1.61.0 # MSRV
             deps: sudo apt update && sudo apt install gcc-multilib
           - target: i686-unknown-linux-gnu
             rust: stable
@@ -57,7 +57,7 @@ jobs:
 
           # 64-bit Linux
           - target: x86_64-unknown-linux-gnu
-            rust: 1.57.0 # MSRV
+            rust: 1.61.0 # MSRV
           - target: x86_64-unknown-linux-gnu
             rust: stable
     steps:
@@ -122,7 +122,7 @@ jobs:
       - uses: actions/checkout@v3
       - uses: actions-rs/toolchain@v1
         with:
-          toolchain: 1.57.0
+          toolchain: 1.61.0
           components: clippy
           override: true
           profile: minimal

src/uint/add.rs
@@ -1,6 +1,6 @@
 //! [`UInt`] addition operations.
 
-use crate::{Checked, CheckedAdd, Limb, UInt, Wrapping, Zero};
+use crate::{Checked, CheckedAdd, Limb, UInt, Word, Wrapping, Zero};
 use core::ops::{Add, AddAssign};
 use subtle::CtOption;
 
@@ -35,6 +35,14 @@ impl<const LIMBS: usize> UInt<LIMBS> {
     /// Perform wrapping addition, discarding overflow.
     pub const fn wrapping_add(&self, rhs: &Self) -> Self {
         self.adc(rhs, Limb::ZERO).0
+    }
+
+    /// Perform wrapping addition, returning the overflow bit as a `Word` that is either 0...0 or 1...1.
+    pub(crate) const fn conditional_wrapping_add(&self, rhs: &Self, choice: Word) -> (Self, Word) {
+        let actual_rhs = UInt::ct_select(UInt::ZERO, *rhs, choice);
+        let (sum, carry) = self.adc(&actual_rhs, Limb::ZERO);
+
+        (sum, carry.0.wrapping_mul(Word::MAX))
     }
 }
 

src/uint/bit_and.rs
@@ -46,6 +46,7 @@ impl<const LIMBS: usize> BitAnd for UInt<LIMBS> {
 impl<const LIMBS: usize> BitAnd<&UInt<LIMBS>> for UInt<LIMBS> {
     type Output = UInt<LIMBS>;
 
+    #[allow(clippy::needless_borrow)]
     fn bitand(self, rhs: &UInt<LIMBS>) -> UInt<LIMBS> {
         (&self).bitand(rhs)
     }

src/uint/bit_not.rs
@@ -23,6 +23,7 @@ impl<const LIMBS: usize> UInt<LIMBS> {
 impl<const LIMBS: usize> Not for UInt<LIMBS> {
     type Output = Self;
 
+    #[allow(clippy::needless_borrow)]
     fn not(self) -> <Self as Not>::Output {
         (&self).not()
     }

src/uint/bit_or.rs
@@ -46,6 +46,7 @@ impl<const LIMBS: usize> BitOr for UInt<LIMBS> {
 impl<const LIMBS: usize> BitOr<&UInt<LIMBS>> for UInt<LIMBS> {
     type Output = UInt<LIMBS>;
 
+    #[allow(clippy::needless_borrow)]
     fn bitor(self, rhs: &UInt<LIMBS>) -> UInt<LIMBS> {
         (&self).bitor(rhs)
     }

commit dfae10268bb14234c94206e3f097d8b9fabe2c97

Bump version to `v0.5.0-pre`; remove deprecations
Breaking changes were introduced in #130, so this bumps the crate's
version to a prerelease to denote that. It will be bumped to `pre.0`
for an actual crate release.

Also removes all deprecated APIs.

Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "crypto-bigint"
-version = "0.4.9"
+version = "0.5.0-pre"
 description = """
 Pure Rust implementation of a big integer library which has been designed from
 the ground-up for use in cryptographic applications. Provides constant-time,

src/lib.rs
@@ -177,10 +177,6 @@ pub use crate::{
 };
 pub use subtle;
 
-// TODO(tarcieri): remove these in the next breaking release
-#[allow(deprecated)]
-pub use crate::limb::{LimbUInt, WideLimbUInt};
-
 pub(crate) use limb::{SignedWord, WideSignedWord};
 
 #[cfg(feature = "generic-array")]

src/limb.rs
@@ -70,20 +70,6 @@ pub type WideWord = u128;
 #[cfg(target_pointer_width = "64")]
 pub(crate) type WideSignedWord = i128;
 
-//
-// Deprecated legacy names
-//
-
-// TODO(tarcieri): remove these in the next breaking release
-
-/// Deprecated: unsigned integer type that the [`Limb`] newtype wraps.
-#[deprecated(since = "0.4.8", note = "please use `Word` instead")]
-pub type LimbUInt = Word;
-
-/// Deprecated: wide integer type which is double the width of [`Word`].
-#[deprecated(since = "0.4.8", note = "please use `WideWord` instead")]
-pub type WideLimbUInt = WideWord;
-
 /// Highest bit in a [`Limb`].
 pub(crate) const HI_BIT: usize = Limb::BIT_SIZE - 1;
 

src/uint.rs
@@ -145,24 +145,6 @@ impl<const LIMBS: usize> UInt<LIMBS> {
         }
     }
 
-    /// Deprecated: borrow the inner limbs as an array of [`Word`]s.
-    #[deprecated(since = "0.4.8", note = "please use `as_words` instead")]
-    pub const fn as_uint_array(&self) -> &[Word; LIMBS] {
-        self.as_words()
-    }
-
-    /// Deprecated: create a [`UInt`] from an array of [`Word`]s.
-    #[deprecated(since = "0.4.8", note = "please use `from_words` instead")]
-    pub const fn from_uint_array(words: [Word; LIMBS]) -> Self {
-        Self::from_words(words)
-    }
-
-    /// Deprecated: create an array of [`Word`]s from a [`UInt`].
-    #[deprecated(since = "0.4.8", note = "please use `to_words` instead")]
-    pub const fn to_uint_array(self) -> [Word; LIMBS] {
-        self.to_words()
-    }
-
     /// Borrow the limbs of this [`UInt`].
     // TODO(tarcieri): rename to `as_limbs` for consistency with `as_words`
     pub const fn limbs(&self) -> &[Limb; LIMBS] {

src/uint/bits.rs
@@ -13,13 +13,6 @@ impl<const LIMBS: usize> UInt<LIMBS> {
     }
 
     /// Calculate the number of bits needed to represent this number.
-    #[deprecated(note = "please use `bits_vartime` instead")]
-    #[inline(always)]
-    pub const fn bits(self) -> usize {
-        self.bits_vartime()
-    }
-
-    /// Calculate the number of bits needed to represent this number.
     #[allow(trivial_numeric_casts)]
     pub const fn bits_vartime(self) -> usize {
         let mut i = LIMBS - 1;