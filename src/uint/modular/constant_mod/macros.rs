6e5213b887766c91a6a381c61db359e31a0b0893
Ola Bini <52427592+olabiniV2@users.noreply.github.com>
2023-03-06 21:53:44 +0000

    Fix #191 by using a module path available from the outside of the create (#193)
    Fix #191 by using a module path that is available from outside of the crate,
    and add a test to make sure the macro const_residue is possible to use
    outside of the crate
    
    Co-authored-by: Ola Bini <ola@olabini.se>

--- src/uint/modular/constant_mod/macros.rs
+++ src/uint/modular/constant_mod/macros.rs
@@ -45,8 +45,6 @@ macro_rules! impl_modulus {
 /// For example, `residue!(U256::from(105u64), MyModulus);` creates a `Residue` for 105 mod `MyModulus`.
 macro_rules! const_residue {
     ($variable:ident, $modulus:ident) => {
-        $crate::uint::modular::constant_mod::Residue::<$modulus, { $modulus::LIMBS }>::new(
-            &$variable,
-        )
+        $crate::modular::constant_mod::Residue::<$modulus, { $modulus::LIMBS }>::new(&$variable)
     };
 }
