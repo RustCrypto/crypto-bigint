f661aa6687774ef03d0c4191f97a1a469cfdcf8e
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-04-30 19:08:53 +0300

    Switched to From instead of to_uxxx

--- src/uint/convert.rs
+++ src/uint/convert.rs
@@ -1,22 +1,34 @@
 macro_rules! impl_convert {
     (($source_type:ident, $source_bits:expr), ($(($target_type:ident, $target_bits:expr)),+ $(,)?)) => {
-        impl $source_type {
-            $(
-                paste::paste! {
-                    pub fn [<to_ $target_type:lower>](&self) -> $target_type {
-                        let mut limbs = [Limb::ZERO; nlimbs!($target_bits)];
-                        let mut i = 0;
+        $(
+            impl From<$source_type> for $target_type {
+                fn from(x: $source_type) -> Self {
+                    let mut limbs = [Limb::ZERO; nlimbs!($target_bits)];
+                    let mut i = 0;
 
-                        while i < nlimbs!($source_bits) {
-                            limbs[i] = self.limbs[i];
-                            i += 1;
-                        }
-
-                        Uint { limbs }
+                    while i < nlimbs!($source_bits) {
+                        limbs[i] = x.limbs[i];
+                        i += 1;
                     }
+
+                    Uint { limbs }
                 }
-            )+
-        }
+            }
+
+            impl From<&$source_type> for $target_type {
+                fn from(x: &$source_type) -> Self {
+                    let mut limbs = [Limb::ZERO; nlimbs!($target_bits)];
+                    let mut i = 0;
+
+                    while i < nlimbs!($source_bits) {
+                        limbs[i] = x.limbs[i];
+                        i += 1;
+                    }
+
+                    Uint { limbs }
+                }
+            }
+        )+
     };
 }
 
@@ -28,11 +40,13 @@ mod tests {
     fn concat_zero_equals_convert() {
         let x = U64::from_u64(0x0011223344556677);
 
-        assert_eq!(U64::ZERO.concat(&x), U128::from_u64(0x0011223344556677));
+        assert_eq!(U128::from(&x), U128::from_u64(0x0011223344556677));
+        assert_eq!(U128::from(x), U128::from_u64(0x0011223344556677));
     }
 
     #[test]
     fn converts_non_concatable_types() {
-        assert_eq!(U384::ONE, U128::ONE.to_u384());
+        assert_eq!(U384::ONE, U384::from(&U128::ONE));
+        assert_eq!(U384::ONE, U384::from(U128::ONE));
     }
 }
