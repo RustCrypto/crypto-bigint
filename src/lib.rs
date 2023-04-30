ef837b8cea0b9fc1b5d89bb5965bc6c51cae1cad
Tony Arcieri <bascule@gmail.com>
2023-04-30 10:26:01 -0600

    Add `BoxedUint`: heap-allocated fixed-precision integers
    Use cases like DSA and RSA benefit from having heap-allocated integers
    whose precision can be chosen at runtime rather than compile time.
    
    These algorithms are used in conjunction with varying key sizes, and
    when dealing with a large number of potential key sizes it's helpful not
    to have to monomorphize the algorithm implementation for each one.
    
    Ideally we can define traits so that code can be written generically but
    used with either `Uint` or `BoxedUint`, allowing heapless `no_std` users
    to use e.g. `U2048`, while users with a heap can use `BoxedUint`.
    
    It should also be possible to make some of the existing algorithm
    implementations generic over `Uint` vs `BoxedUint`.
    
    This initial implementation only provides addition and comparison
    support, but also the initial scaffolding for adding more operations.

--- src/lib.rs
+++ src/lib.rs
@@ -151,7 +151,7 @@
 //! [`Rem`]: core::ops::Rem
 //! [`Sub`]: core::ops::Sub
 
-#[cfg(all(feature = "alloc", test))]
+#[cfg(feature = "alloc")]
 extern crate alloc;
 
 #[macro_use]
@@ -159,6 +159,8 @@ mod nlimbs;
 
 #[cfg(feature = "generic-array")]
 mod array;
+#[cfg(feature = "alloc")]
+mod boxed;
 mod checked;
 mod ct_choice;
 mod limb;
@@ -178,6 +180,9 @@ pub use crate::{
     wrapping::Wrapping,
 };
 pub use subtle;
+
+#[cfg(feature = "alloc")]
+pub use crate::boxed::uint::BoxedUint;
 
 #[cfg(feature = "generic-array")]
 pub use {
