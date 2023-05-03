773fbf1b222b2326be94a415d0a285f715acdfe2
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-05-03 13:45:48 +0300

    Implemented all types

--- src/uint/mul.rs
+++ src/uint/mul.rs
@@ -296,36 +296,68 @@ macro_rules! impl_mul {
 }
 
 macro_rules! impl_mul_cross_sizes {
-    (($self_type:ident, $self_bits:expr), ($(($rhs_type:ident, $rhs_bits:expr)),+ $(,)?)) => {
+    (($first_type:ident, $first_bits:expr), ($(($second_type:ident, $second_bits:expr)),+ $(,)?)) => {
         $(
-            impl Mul<$rhs_type> for $self_type {
-                type Output = Uint<{nlimbs!($self_bits) + nlimbs!($rhs_bits)}>;
+            impl Mul<$second_type> for $first_type {
+                type Output = Uint<{nlimbs!($first_bits) + nlimbs!($second_bits)}>;
 
-                fn mul(self, rhs: $rhs_type) -> Uint<{nlimbs!($self_bits) + nlimbs!($rhs_bits)}> {
+                fn mul(self, rhs: $second_type) -> Uint<{nlimbs!($first_bits) + nlimbs!($second_bits)}> {
                     self.mul_wide(&rhs).into()
                 }
             }
 
-            impl Mul<&$rhs_type> for $self_type  {
-                type Output = Uint<{nlimbs!($self_bits) + nlimbs!($rhs_bits)}>;
+            impl Mul<&$second_type> for $first_type  {
+                type Output = Uint<{nlimbs!($first_bits) + nlimbs!($second_bits)}>;
 
-                fn mul(self, rhs: &$rhs_type) -> Uint<{nlimbs!($self_bits) + nlimbs!($rhs_bits)}> {
+                fn mul(self, rhs: &$second_type) -> Uint<{nlimbs!($first_bits) + nlimbs!($second_bits)}> {
                     self.mul_wide(rhs).into()
                 }
             }
 
-            impl Mul<$rhs_type> for &$self_type  {
-                type Output = Uint<{nlimbs!($self_bits) + nlimbs!($rhs_bits)}>;
+            impl Mul<$second_type> for &$first_type  {
+                type Output = Uint<{nlimbs!($first_bits) + nlimbs!($second_bits)}>;
 
-                fn mul(self, rhs: $rhs_type) -> Uint<{nlimbs!($self_bits) + nlimbs!($rhs_bits)}> {
+                fn mul(self, rhs: $second_type) -> Uint<{nlimbs!($first_bits) + nlimbs!($second_bits)}> {
                     self.mul_wide(&rhs).into()
                 }
             }
 
-            impl Mul<&$rhs_type> for &$self_type {
-                type Output = Uint<{nlimbs!($self_bits) + nlimbs!($rhs_bits)}>;
+            impl Mul<&$second_type> for &$first_type {
+                type Output = Uint<{nlimbs!($first_bits) + nlimbs!($second_bits)}>;
 
-                fn mul(self, rhs: &$rhs_type) -> Uint<{nlimbs!($self_bits) + nlimbs!($rhs_bits)}> {
+                fn mul(self, rhs: &$second_type) -> Uint<{nlimbs!($first_bits) + nlimbs!($second_bits)}> {
+                    self.mul_wide(rhs).into()
+                }
+            }
+
+            impl Mul<$first_type> for $second_type {
+                type Output = Uint<{nlimbs!($second_bits) + nlimbs!($first_bits)}>;
+
+                fn mul(self, rhs: $first_type) -> Uint<{nlimbs!($second_bits) + nlimbs!($first_bits)}> {
+                    self.mul_wide(&rhs).into()
+                }
+            }
+
+            impl Mul<&$first_type> for $second_type  {
+                type Output = Uint<{nlimbs!($second_bits) + nlimbs!($first_bits)}>;
+
+                fn mul(self, rhs: &$first_type) -> Uint<{nlimbs!($second_bits) + nlimbs!($first_bits)}> {
+                    self.mul_wide(rhs).into()
+                }
+            }
+
+            impl Mul<$first_type> for &$second_type  {
+                type Output = Uint<{nlimbs!($second_bits) + nlimbs!($first_bits)}>;
+
+                fn mul(self, rhs: $first_type) -> Uint<{nlimbs!($second_bits) + nlimbs!($first_bits)}> {
+                    self.mul_wide(&rhs).into()
+                }
+            }
+
+            impl Mul<&$first_type> for &$second_type {
+                type Output = Uint<{nlimbs!($second_bits) + nlimbs!($first_bits)}>;
+
+                fn mul(self, rhs: &$first_type) -> Uint<{nlimbs!($second_bits) + nlimbs!($first_bits)}> {
                     self.mul_wide(rhs).into()
                 }
             }
@@ -335,7 +367,7 @@ macro_rules! impl_mul_cross_sizes {
 
 #[cfg(test)]
 mod tests {
-    use crate::{CheckedMul, Zero, U128, U256, U384, U64};
+    use crate::{CheckedMul, Uint, Zero, U128, U256, U384, U4096, U64};
 
     #[test]
     fn mul_wide_zero_and_one() {
@@ -445,6 +477,7 @@ mod tests {
         let xy: U384 = x.mul_wide(&y).into();
 
         assert_eq!(xy, x * y);
+        assert_eq!(xy, y * x);
 
         assert_eq!(
             xy,
