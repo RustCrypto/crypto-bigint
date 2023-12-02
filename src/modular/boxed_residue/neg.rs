67d151ba35185241f5357e01ec529da65c4db8d3
Tony Arcieri <bascule@gmail.com>
2023-12-02 10:55:15 -0700

    Add `std` feature
    - Impl `std::error::Error` for `DecodeError`
    - Use `Arc` for storing `BoxedResidueParams` when available

--- src/modular/boxed_residue/neg.rs
+++ src/modular/boxed_residue/neg.rs
@@ -1,12 +1,18 @@
 //! Negations of boxed residues.
 
 use super::BoxedResidue;
+use crate::BoxedUint;
 use core::ops::Neg;
 
 impl BoxedResidue {
     /// Negates the number.
     pub fn neg(&self) -> Self {
-        Self::zero(self.residue_params.clone()).sub(self)
+        let zero = Self {
+            montgomery_form: BoxedUint::zero_with_precision(self.residue_params.bits_precision()),
+            residue_params: self.residue_params.clone(),
+        };
+
+        zero.sub(self)
     }
 }
 
