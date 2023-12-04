14c6d3bb2888bf651f42aeb093492938490ae8f5
Tony Arcieri <bascule@gmail.com>
2023-12-04 08:27:31 -0700

    BoxedUint: improve efficiency of `square`
    Uses a more efficient squaring algorithm with fewer iterations, with an
    implementation shared with `Uint`.
    
    Proptested against `num-bigint`.

--- src/uint/boxed/mul.rs
+++ src/uint/boxed/mul.rs
@@ -1,6 +1,9 @@
 //! [`BoxedUint`] multiplication operations.
 
-use crate::{uint::mul::mul_limbs, BoxedUint, CheckedMul, Limb, WideningMul, Wrapping, Zero};
+use crate::{
+    uint::mul::{diagonal_mul_limbs, half_mul_limbs, mul_limbs},
+    BoxedUint, CheckedMul, Limb, WideningMul, Wrapping, Zero,
+};
 use core::ops::{Mul, MulAssign};
 use subtle::{Choice, CtOption};
 
@@ -9,9 +12,9 @@ impl BoxedUint {
     ///
     /// Returns a widened output with a limb count equal to the sums of the input limb counts.
     pub fn mul(&self, rhs: &Self) -> Self {
-        let mut limbs = vec![Limb::ZERO; self.nlimbs() + rhs.nlimbs()];
-        mul_limbs(&self.limbs, &rhs.limbs, &mut limbs);
-        limbs.into()
+        let mut out = vec![Limb::ZERO; self.nlimbs() + rhs.nlimbs()];
+        mul_limbs(&self.limbs, &rhs.limbs, &mut out);
+        out.into()
     }
 
     /// Perform wrapping multiplication, wrapping to the width of `self`.
@@ -21,8 +24,11 @@ impl BoxedUint {
 
     /// Multiply `self` by itself.
     pub fn square(&self) -> Self {
-        // TODO(tarcieri): more optimized implementation (shared with `Uint`?)
-        self.mul(self)
+        let mut out = Self::from(vec![Limb::ZERO; self.nlimbs() * 2]);
+        half_mul_limbs(&self.limbs, &mut out.limbs);
+        out <<= 1;
+        diagonal_mul_limbs(&self.limbs, &mut out.limbs);
+        out
     }
 }
 
