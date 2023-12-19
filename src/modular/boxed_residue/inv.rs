8c6ecaae5f31d333a3f2d67d290954f5f7b109ff
Tony Arcieri <bascule@gmail.com>
2023-12-18 17:42:30 -0700

    Remove `BoxedBernsteinYangInverter`
    Per #468 there are undiagnosed issues with it which aren't occurring
    with e.g. `DynResidueInverter`, which indicates some kind of
    implementation defect.
    
    Until such time as it can be diagnosed, this commit is removing the
    implementation to be on the safe side.

--- src/modular/boxed_residue/inv.rs
+++ src/modular/boxed_residue/inv.rs
@@ -1,15 +1,8 @@
 //! Multiplicative inverses of boxed residue.
 
-use super::{BoxedResidue, BoxedResidueParams};
-use crate::{
-    modular::{reduction::montgomery_reduction_boxed_mut, BoxedBernsteinYangInverter},
-    Invert, Inverter, PrecomputeInverter, PrecomputeInverterWithAdjuster,
-};
-use core::fmt;
+use super::BoxedResidue;
+use crate::{modular::reduction::montgomery_reduction_boxed_mut, Invert};
 use subtle::CtOption;
-
-#[cfg(feature = "std")]
-use std::sync::Arc;
 
 impl BoxedResidue {
     /// Computes the residue `self^-1` representing the multiplicative inverse of `self`.
@@ -44,63 +37,12 @@ impl Invert for BoxedResidue {
     }
 }
 
-impl PrecomputeInverter for BoxedResidueParams {
-    type Inverter = BoxedResidueInverter;
-    type Output = BoxedResidue;
-
-    fn precompute_inverter(&self) -> BoxedResidueInverter {
-        BoxedResidueInverter {
-            inverter: self.modulus.precompute_inverter_with_adjuster(&self.r2),
-            residue_params: self.clone().into(),
-        }
-    }
-}
-
-/// Bernstein-Yang inverter which inverts [`DynResidue`] types.
-pub struct BoxedResidueInverter {
-    /// Precomputed Bernstein-Yang inverter.
-    inverter: BoxedBernsteinYangInverter,
-
-    /// Residue parameters.
-    #[cfg(not(feature = "std"))]
-    residue_params: BoxedResidueParams,
-
-    /// Residue parameters.
-    // Uses `Arc` when `std` is available.
-    #[cfg(feature = "std")]
-    residue_params: Arc<BoxedResidueParams>,
-}
-
-impl Inverter for BoxedResidueInverter {
-    type Output = BoxedResidue;
-
-    fn invert(&self, value: &BoxedResidue) -> CtOption<Self::Output> {
-        debug_assert_eq!(self.residue_params, value.residue_params);
-
-        let montgomery_form = self.inverter.invert(&value.montgomery_form);
-        let is_some = montgomery_form.is_some();
-        let montgomery_form2 = value.montgomery_form.clone();
-        let ret = BoxedResidue {
-            montgomery_form: Option::from(montgomery_form).unwrap_or(montgomery_form2),
-            residue_params: value.residue_params.clone(),
-        };
-
-        CtOption::new(ret, is_some)
-    }
-}
-
-impl fmt::Debug for BoxedResidueInverter {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("BoxedResidueInverter")
-            .field("modulus", &self.inverter.modulus)
-            .finish()
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use super::{BoxedResidue, BoxedResidueParams};
-    use crate::{BoxedUint, Inverter, PrecomputeInverter};
+    use crate::{
+        modular::{BoxedResidue, BoxedResidueParams},
+        BoxedUint,
+    };
     use hex_literal::hex;
 
     fn residue_params() -> BoxedResidueParams {
@@ -125,23 +67,6 @@ mod tests {
         let x_mod = BoxedResidue::new(x, params);
 
         let inv = x_mod.invert().unwrap();
-        let res = x_mod * inv;
-
-        assert!(bool::from(res.retrieve().is_one()));
-    }
-
-    #[test]
-    fn test_self_inverse_precomuted() {
-        let params = residue_params();
-        let inverter = params.precompute_inverter();
-
-        let x = BoxedUint::from_be_slice(
-            &hex!("77117F1273373C26C700D076B3F780074D03339F56DD0EFB60E7F58441FD3685"),
-            256,
-        )
-        .unwrap();
-        let x_mod = BoxedResidue::new(x, params);
-        let inv = inverter.invert(&x_mod).unwrap();
         let res = x_mod * inv;
 
         assert!(bool::from(res.retrieve().is_one()));
