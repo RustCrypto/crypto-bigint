7f17c5a2d76cc966b27cf893698c28f8a536b3e8
pinkforest <36498018+pinkforest@users.noreply.github.com>
2024-04-06 15:25:39 +1100

    Move under encoding and address other things

---
+++ src/uint/boxed/encoding/serde.rs
@@ -0,0 +1,63 @@
+//! Support for serdect encoding/decoding [`BoxedUint`]
+
+use serdect::serde::{de, Deserialize, Deserializer, Serialize, Serializer};
+
+use crate::BoxedUint;
+
+impl<'de> Deserialize<'de> for BoxedUint {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let mut buffer = Self::zero().to_le_bytes();
+        serdect::array::deserialize_hex_or_bin(buffer.as_mut(), deserializer)?;
+
+        let bits_in = buffer.len() * 8;
+
+        if bits_in > u32::MAX as usize {
+            return Err(de::Error::custom(
+                "Deserialize input overflows BoxedUint u32 bits length",
+            ));
+        }
+
+        Self::from_le_slice(&buffer, bits_in as u32)
+            .map_err(|_| de::Error::custom("Deserialize error"))
+    }
+}
+
+#[cfg(feature = "serde")]
+impl Serialize for BoxedUint {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serdect::array::serialize_hex_lower_or_bin(&self.to_le_bytes(), serializer)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::BoxedUint;
+
+    #[test]
+    fn serde() {
+        #[allow(trivial_numeric_casts)]
+        let test: BoxedUint = BoxedUint::from(0x0011223344556677 as u64);
+
+        let serialized = bincode::serialize(&test).unwrap();
+        let deserialized: BoxedUint = bincode::deserialize(&serialized).unwrap();
+
+        assert_eq!(test, deserialized);
+    }
+
+    #[test]
+    fn serde_owned() {
+        #[allow(trivial_numeric_casts)]
+        let test: BoxedUint = BoxedUint::from(0x0011223344556677 as u64);
+
+        let serialized = bincode::serialize(&test).unwrap();
+        let deserialized: BoxedUint = bincode::deserialize_from(serialized.as_slice()).unwrap();
+
+        assert_eq!(test, deserialized);
+    }
+}
