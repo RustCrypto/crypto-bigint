612ba637e971b9e834aff655fd265440d49864af
Andrew Whitehead <cywolf@gmail.com>
2024-08-28 14:00:08 -0700

    test BoxedMontyForm neg method
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/modular/boxed_monty_form/neg.rs
+++ src/modular/boxed_monty_form/neg.rs
@@ -26,3 +26,31 @@ impl Neg for &BoxedMontyForm {
         BoxedMontyForm::neg(self)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        modular::{BoxedMontyForm, BoxedMontyParams},
+        BoxedUint,
+    };
+    use hex_literal::hex;
+
+    #[test]
+    fn neg_expected() {
+        let modulus = BoxedUint::from_be_slice(
+            &hex!("ffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551"),
+            256,
+        )
+        .expect("error creating modulus");
+        let params = BoxedMontyParams::new(modulus.to_odd().unwrap());
+
+        let x = BoxedUint::from_be_slice(
+            &hex!("44acf6b7e36c1342c2c5897204fe09504e1e2efb1a900377dbc4e7a6a133ec56"),
+            256,
+        )
+        .expect("error creating boxeduint");
+        let x_mod = BoxedMontyForm::new(x, params.clone());
+
+        assert!(bool::from((x_mod.neg() + x_mod).is_zero()));
+    }
+}
