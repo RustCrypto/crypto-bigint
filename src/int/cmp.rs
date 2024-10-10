e38062a7ebc904b19b569ea9db07f6633681bb2c
Erik Takke <erik.takke@3milabs.tech>
2024-10-10 12:39:55 +0200

    Introduce Int::eq

--- src/int/cmp.rs
+++ src/int/cmp.rs
@@ -2,9 +2,10 @@
 //!
 //! By default, these are all constant-time and use the `subtle` crate.
 
-use subtle::{ConditionallySelectable, ConstantTimeLess};
+use subtle::{Choice, ConditionallySelectable, ConstantTimeEq, ConstantTimeLess};
 
 use crate::int::Int;
+use crate::Uint;
 
 impl<const LIMBS: usize> Int<LIMBS> {
     /// Given two [`Int`]s, return a tuple with the larger magnitude first.
@@ -16,5 +17,19 @@ impl<const LIMBS: usize> Int<LIMBS> {
         let greatest = Int::conditional_select(self, rhs, swap);
         let smallest = Int::conditional_select(rhs, self, swap);
         (greatest, smallest)
+    }
+
+    #[inline]
+    /// TODO: make this a const function?
+    pub(crate) fn eq(lhs: &Self, rhs: &Self) -> Choice {
+        Choice::from(Uint::eq(&lhs.magnitude, &rhs.magnitude))
+            & Choice::ct_eq(&lhs.is_negative(), &rhs.is_negative())
+    }
+}
+
+impl<const LIMBS: usize> ConstantTimeEq for Int<LIMBS> {
+    #[inline]
+    fn ct_eq(&self, other: &Self) -> Choice {
+        Int::eq(self, other)
     }
 }
