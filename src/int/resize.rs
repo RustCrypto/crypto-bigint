080c6c88eeaaed9d3f01953ca3a1657edd5e61ae
Erik Takke <erik.takke@3milabs.tech>
2024-10-18 13:32:48 +0200

    Fix `Int::resize` bug

--- src/int/resize.rs
+++ src/int/resize.rs
@@ -1,10 +1,50 @@
-use crate::Int;
+use crate::{Int, Limb, Uint};
 
 impl<const LIMBS: usize> Int<LIMBS> {
     /// Resize the representation of `self` to an `Int<T>`.
     /// Warning: this operation may lead to loss of information.
     #[inline(always)]
     pub const fn resize<const T: usize>(&self) -> Int<T> {
-        Int::new_from_uint(self.0.resize::<T>())
+        let mut limbs = [Limb::select(Limb::ZERO, Limb::MAX, self.is_negative()); T];
+        let mut i = 0;
+        let dim = if T < LIMBS { T } else { LIMBS };
+        while i < dim {
+            limbs[i] = self.0.limbs[i];
+            i += 1;
+        }
+        Int { 0: Uint { limbs } }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use num_traits::WrappingSub;
+
+    use crate::{I128, I256};
+
+    #[test]
+    fn scale_down() {
+        assert_eq!(I256::MIN.resize::<{ I128::LIMBS }>(), I128::ZERO);
+        assert_eq!(I256::MINUS_ONE.resize::<{ I128::LIMBS }>(), I128::MINUS_ONE);
+        assert_eq!(I256::ZERO.resize::<{ I128::LIMBS }>(), I128::ZERO);
+        assert_eq!(I256::ONE.resize::<{ I128::LIMBS }>(), I128::ONE);
+        assert_eq!(I256::MAX.resize::<{ I128::LIMBS }>(), I128::MINUS_ONE);
+    }
+
+    #[test]
+    fn scale_up() {
+        assert_eq!(
+            I128::MIN.resize::<{ I256::LIMBS }>(),
+            I256::ZERO.wrapping_sub(&I256 {
+                0: I128::MIN.0.resize()
+            })
+        );
+        assert_eq!(I128::MINUS_ONE.resize::<{ I256::LIMBS }>(), I256::MINUS_ONE);
+        assert_eq!(I128::ZERO.resize::<{ I256::LIMBS }>(), I256::ZERO);
+        assert_eq!(I128::ONE.resize::<{ I256::LIMBS }>(), I256::ONE);
+        assert_eq!(
+            I128::MAX.resize::<{ I256::LIMBS }>(),
+            I256::new_from_uint(I128::MAX.0.resize())
+        );
     }
 }
