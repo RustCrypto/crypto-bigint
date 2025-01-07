c8ad469e53e77a6a9f9a145514581d7e9946260e
Erik Takke <erik.takke@3milabs.tech>
2025-01-07 17:35:43 +0100

    Introduce `Int::mul_uint`

---
+++ src/int/mul_uint.rs
@@ -0,0 +1,253 @@
+use core::ops::Mul;
+
+use subtle::CtOption;
+
+use crate::{CheckedMul, ConcatMixed, ConstChoice, Int, Uint, Zero};
+
+impl<const LIMBS: usize> Int<LIMBS> {
+    /// Compute "wide" multiplication between an [`Int`] and [`Uint`] as 3-tuple `(lo, hi, negate)`.
+    /// The `(lo, hi)` components contain the _magnitude of the product_, with sizes
+    /// corresponding to the sizes of the operands; `negate` indicates whether the result should be
+    /// negated when converted from [`Uint`] to [`Int`].
+    ///
+    /// Note: even if `negate` is truthy, the magnitude might be zero!
+    pub const fn split_mul_uint<const RHS_LIMBS: usize>(
+        &self,
+        rhs: &Uint<RHS_LIMBS>,
+    ) -> (Uint<{ LIMBS }>, Uint<{ RHS_LIMBS }>, ConstChoice) {
+        // Step 1. split self into its sign and magnitude.
+        let (lhs_abs, lhs_sgn) = self.abs_sign();
+
+        // Step 2. Multiply the magnitudes
+        let (lo, hi) = lhs_abs.split_mul(rhs);
+
+        // Step 3. negate if and only if self has a negative sign.
+        (lo, hi, lhs_sgn)
+    }
+
+    /// Compute "wide" multiplication between an [`Int`] and [`Uint`] as 3-tuple `(lo, hi, negate)`.
+    /// The `(lo, hi)` components contain the _magnitude of the product_, with sizes
+    /// corresponding to the sizes of the operands, in reversed order; `negate` indicates whether
+    /// the result should be negated when converted from [`Uint`] to [`Int`].
+    ///
+    /// Note: even if `negate` is truthy, the magnitude might be zero!
+    pub const fn split_mul_uint_right<const RHS_LIMBS: usize>(
+        &self,
+        rhs: &Uint<RHS_LIMBS>,
+    ) -> (Uint<{ RHS_LIMBS }>, Uint<{ LIMBS }>, ConstChoice) {
+        let (lhs_abs, lhs_sgn) = self.abs_sign();
+        let (lo, hi) = rhs.split_mul(&lhs_abs);
+        (lo, hi, lhs_sgn)
+    }
+
+    /// Multiply `self` by [`Uint`] `rhs`, returning a concatenated "wide" result.
+    pub const fn widening_mul_uint<const RHS_LIMBS: usize, const WIDE_LIMBS: usize>(
+        &self,
+        rhs: &Uint<RHS_LIMBS>,
+    ) -> Int<WIDE_LIMBS>
+    where
+        Uint<LIMBS>: ConcatMixed<Uint<RHS_LIMBS>, MixedOutput = Uint<WIDE_LIMBS>>,
+    {
+        let (lhs_abs, lhs_sign) = self.abs_sign();
+        let product_abs = lhs_abs.widening_mul(rhs);
+
+        // always fits
+        product_abs.wrapping_neg_if(lhs_sign).as_int()
+    }
+
+    /// Checked multiplication of self with an `Uint<RHS_LIMBS>`, where the result is to be stored
+    /// in an `Int<RHS_LIMBS>`.
+    pub fn checked_mul_uint_right<const RHS_LIMBS: usize>(
+        &self,
+        rhs: &Uint<RHS_LIMBS>,
+    ) -> CtOption<Int<RHS_LIMBS>> {
+        let (lo, hi, is_negative) = self.split_mul_uint_right(rhs);
+        let val = Int::<RHS_LIMBS>::new_from_abs_sign(lo, is_negative);
+        CtOption::from(val).and_then(|int| CtOption::new(int, hi.is_zero()))
+    }
+}
+
+impl<const LIMBS: usize, const RHS_LIMBS: usize> CheckedMul<Uint<RHS_LIMBS>> for Int<LIMBS> {
+    #[inline]
+    fn checked_mul(&self, rhs: &Uint<RHS_LIMBS>) -> CtOption<Self> {
+        let (lo, hi, is_negative) = self.split_mul_uint(rhs);
+        let val = Self::new_from_abs_sign(lo, is_negative);
+        CtOption::from(val).and_then(|int| CtOption::new(int, hi.is_zero()))
+    }
+}
+
+impl<const LIMBS: usize, const RHS_LIMBS: usize> Mul<Uint<RHS_LIMBS>> for Int<LIMBS> {
+    type Output = Int<LIMBS>;
+
+    fn mul(self, rhs: Uint<RHS_LIMBS>) -> Self {
+        self.mul(&rhs)
+    }
+}
+
+impl<const LIMBS: usize, const RHS_LIMBS: usize> Mul<&Uint<RHS_LIMBS>> for Int<LIMBS> {
+    type Output = Int<LIMBS>;
+
+    fn mul(self, rhs: &Uint<RHS_LIMBS>) -> Self {
+        (&self).mul(rhs)
+    }
+}
+
+impl<const LIMBS: usize, const RHS_LIMBS: usize> Mul<Uint<RHS_LIMBS>> for &Int<LIMBS> {
+    type Output = Int<LIMBS>;
+
+    fn mul(self, rhs: Uint<RHS_LIMBS>) -> Self::Output {
+        self.mul(&rhs)
+    }
+}
+
+impl<const LIMBS: usize, const RHS_LIMBS: usize> Mul<&Uint<RHS_LIMBS>> for &Int<LIMBS> {
+    type Output = Int<LIMBS>;
+
+    fn mul(self, rhs: &Uint<RHS_LIMBS>) -> Self::Output {
+        self.checked_mul(rhs)
+            .expect("attempted to multiply with overflow")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{CheckedMul, I128, I256, U128, U256};
+
+    #[test]
+    fn test_checked_mul_uint() {
+        // lhs = min
+
+        let result = I128::MIN.checked_mul(&U128::ZERO);
+        assert_eq!(result.unwrap(), I128::ZERO);
+
+        let result = I128::MIN.checked_mul(&U128::ONE);
+        assert_eq!(result.unwrap(), I128::MIN);
+
+        let result = I128::MIN.checked_mul(&U128::MAX);
+        assert!(bool::from(result.is_none()));
+
+        // lhs = -1
+
+        let result = I128::MINUS_ONE.checked_mul(&U128::ZERO);
+        assert_eq!(result.unwrap(), I128::ZERO);
+
+        let result = I128::MINUS_ONE.checked_mul(&U128::ONE);
+        assert_eq!(result.unwrap(), I128::MINUS_ONE);
+
+        let result = I128::MINUS_ONE.checked_mul(&U128::MAX);
+        assert!(bool::from(result.is_none()));
+
+        // lhs = 0
+
+        let result = I128::ZERO.checked_mul(&U128::ZERO);
+        assert_eq!(result.unwrap(), I128::ZERO);
+
+        let result = I128::ZERO.checked_mul(&U128::ONE);
+        assert_eq!(result.unwrap(), I128::ZERO);
+
+        let result = I128::ZERO.checked_mul(&U128::MAX);
+        assert_eq!(result.unwrap(), I128::ZERO);
+
+        // lhs = 1
+
+        let result = I128::ONE.checked_mul(&U128::ZERO);
+        assert_eq!(result.unwrap(), I128::ZERO);
+
+        let result = I128::ONE.checked_mul(&U128::ONE);
+        assert_eq!(result.unwrap(), I128::ONE);
+
+        let result = I128::ONE.checked_mul(&U128::MAX);
+        assert!(bool::from(result.is_none()));
+
+        // lhs = max
+
+        let result = I128::MAX.checked_mul(&U128::ZERO);
+        assert_eq!(result.unwrap(), I128::ZERO);
+
+        let result = I128::MAX.checked_mul(&U128::ONE);
+        assert_eq!(result.unwrap(), I128::MAX);
+
+        let result = I128::MAX.checked_mul(&U128::MAX);
+        assert!(bool::from(result.is_none()));
+    }
+
+    #[test]
+    fn test_checked_mul_uint_right() {
+        // rhs = 0
+        let result = I256::MIN.checked_mul_uint_right(&U128::ZERO);
+        assert!(bool::from(result.is_some()));
+        assert_eq!(result.unwrap(), I128::ZERO);
+
+        let result = I128::MIN.checked_mul_uint_right(&U256::ZERO);
+        assert!(bool::from(result.is_some()));
+        assert_eq!(result.unwrap(), I256::ZERO);
+
+        // rhs = 1
+        let result = I256::MIN.checked_mul_uint_right(&U128::ONE);
+        assert!(bool::from(result.is_none()));
+
+        let result = I128::MIN.checked_mul_uint_right(&U256::ONE);
+        assert!(bool::from(result.is_some()));
+        assert_eq!(result.unwrap(), I128::MIN.resize());
+
+        // rhs > 1
+        let result = I256::ONE.checked_mul_uint_right(&I128::MAX.as_uint());
+        assert!(bool::from(result.is_some()));
+        assert_eq!(result.unwrap(), I128::MAX.resize());
+
+        let result = I128::ONE.checked_mul_uint_right(&I256::MAX.as_uint());
+        assert!(bool::from(result.is_some()));
+        assert_eq!(result.unwrap(), I256::MAX);
+
+        // rhs = MAX
+        let result = I256::ONE.checked_mul_uint_right(&U128::MAX);
+        assert!(bool::from(result.is_none()));
+
+        let result = I128::MIN.checked_mul_uint_right(&U256::MAX);
+        assert!(bool::from(result.is_none()));
+    }
+
+    #[test]
+    fn test_widening_mul_uint() {
+        assert_eq!(I128::MIN.widening_mul_uint(&U128::ZERO), I256::ZERO);
+        assert_eq!(
+            I128::MIN.widening_mul_uint(&U128::ONE),
+            I256::from_be_hex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF80000000000000000000000000000000")
+        );
+        assert_eq!(
+            I128::MIN.widening_mul_uint(&U128::MAX),
+            I256::from_be_hex("8000000000000000000000000000000080000000000000000000000000000000")
+        );
+
+        assert_eq!(I128::MINUS_ONE.widening_mul_uint(&U128::ZERO), I256::ZERO);
+        assert_eq!(
+            I128::MINUS_ONE.widening_mul_uint(&U128::ONE),
+            I256::MINUS_ONE
+        );
+        assert_eq!(
+            I128::MINUS_ONE.widening_mul_uint(&U128::MAX),
+            I256::from_be_hex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000000000000000000000000001")
+        );
+
+        assert_eq!(I128::ZERO.widening_mul_uint(&U128::ZERO), I256::ZERO);
+        assert_eq!(I128::ZERO.widening_mul_uint(&U128::ONE), I256::ZERO);
+        assert_eq!(I128::ZERO.widening_mul_uint(&U128::MAX), I256::ZERO);
+
+        assert_eq!(I128::ONE.widening_mul_uint(&U128::ZERO), I256::ZERO);
+        assert_eq!(I128::ONE.widening_mul_uint(&U128::ONE), I256::ONE);
+        assert_eq!(
+            I128::ONE.widening_mul_uint(&U128::MAX),
+            I256::from_be_hex("00000000000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
+        );
+
+        assert_eq!(I128::MAX.widening_mul_uint(&U128::ZERO), I256::ZERO);
+        assert_eq!(
+            I128::MAX.widening_mul_uint(&U128::ONE),
+            I256::from_be_hex("000000000000000000000000000000007FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
+        );
+        assert_eq!(
+            I128::MAX.widening_mul_uint(&U128::MAX),
+            I256::from_be_hex("7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE80000000000000000000000000000001")
+        );
+    }
+}
