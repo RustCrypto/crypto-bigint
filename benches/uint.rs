dd28ce4988563d6f1948eae52a508f6a0b0ed827
Erik Takke <erik.takke@3milabs.tech>
2025-01-27 18:46:21 +0100

    Fix benches

--- benches/uint.rs
+++ benches/uint.rs
@@ -529,14 +529,14 @@ fn bench_sqrt(c: &mut Criterion) {
 
 criterion_group!(
     benches,
-    // bench_random,
-    // bench_mul,
-    // bench_division,
-    // bench_gcd,
-    // bench_shl,
+    bench_random,
+    bench_mul,
+    bench_division,
+    bench_gcd,
+    bench_shl,
     bench_shr,
-    // bench_inv_mod,
-    // bench_sqrt
+    bench_inv_mod,
+    bench_sqrt
 );
 
 criterion_main!(benches);
