a824a4d7ab8176dedac4c116ddb187a7bb12f94b
Erik Takke <erik.takke@3milabs.tech>
2025-01-29 17:01:22 +0100

    Annotate `new_gcd.rs`

--- src/uint/new_gcd.rs
+++ src/uint/new_gcd.rs
@@ -1,3 +1,7 @@
+//! This module implements (a constant variant of) the Optimized Extended Binary GCD algorithm,
+//! which is described by Pornin as Algorithm 2 in "Optimized Binary GCD for Modular Inversion".
+//! Ref: https://eprint.iacr.org/2020/972.pdf
+
 use crate::{ConstChoice, ConstCtOption, Int, Limb, Odd, Uint, I64, U128};
 use num_traits::WrappingSub;
 
