319a1fa47787a83f20bbefd5f22451d83ee2cd80
Erik Takke <erik.takke@3milabs.tech>
2025-02-25 09:55:11 +0100

    Make random_bits_core platform independent

--- src/uint/rand.rs
+++ src/uint/rand.rs
@@ -39,12 +39,26 @@ pub(crate) fn random_bits_core<R: TryRngCore + ?Sized>(
     for i in 0..nonzero_limbs - 1 {
         rng.try_fill_bytes(&mut buffer)
             .map_err(RandomBitsError::RandCore)?;
-        zeroed_limbs[i] = Limb(Word::from_be_bytes(buffer));
+        zeroed_limbs[i] = Limb(Word::from_le_bytes(buffer));
     }
 
-    rng.try_fill_bytes(&mut buffer)
+    // This algorithm should sample the same number of random bytes, regardless of the pointer width
+    // of the target platform. To this end, special attention has to be paid to the case where
+    // bit_length - 1 < 32 mod 64. Bit strings of that size can be represented using `2X+1` 32-bit
+    // words or `X+1` 64-bit words. Note that 64*(X+1) - 32*(2X+1) = 32. Hence, if we sample full
+    // words only, a 64-bit platform will sample 32 bits more than a 32-bit platform. We prevent
+    // this by forcing both platforms to only sample 4 bytes for the last word in this case.
+    let slice = if partial_limb > 0 && partial_limb <= 32 {
+        // Note: we do not have to zeroize the second half of the buffer, as the mask will take
+        // care of this in the end.
+        &mut buffer[0..4]
+    } else {
+        buffer.as_mut_slice()
+    };
+
+    rng.try_fill_bytes(slice)
         .map_err(RandomBitsError::RandCore)?;
-    zeroed_limbs[nonzero_limbs - 1] = Limb(Word::from_be_bytes(buffer) & mask);
+    zeroed_limbs[nonzero_limbs - 1] = Limb(Word::from_le_bytes(buffer) & mask);
 
     Ok(())
 }
@@ -144,9 +158,11 @@ where
 
 #[cfg(test)]
 mod tests {
-    use crate::{Limb, NonZero, RandomBits, RandomMod, U256};
+    use crate::uint::rand::random_bits_core;
+    use crate::{Limb, NonZero, RandomBits, RandomMod, Uint, U1024, U256};
+    use rand_chacha::{ChaCha20Core, ChaCha20Rng};
     use rand_chacha::ChaCha8Rng;
-    use rand_core::SeedableRng;
+    use rand_core::{RngCore, SeedableRng};
 
     #[test]
     fn random_mod() {
@@ -218,5 +234,35 @@ mod tests {
             let res = U256::random_bits(&mut rng, bit_length);
             assert_eq!(res, U256::ZERO);
         }
+    }
+
+    /// Make sure the random_bits output is consistent across platforms
+    #[test]
+    fn random_bits_platform_independence() {
+        let zero_seed = [0u8; 32];
+        let mut rng = ChaCha20Rng::from(ChaCha20Core::from_seed(zero_seed));
+
+        let mut val = U1024::ZERO;
+        let bytes = val.as_limbs_mut().as_mut_slice();
+        random_bits_core(&mut rng, bytes, 989).expect("safe");
+
+        assert_eq!(
+            val,
+            Uint::from_be_hex(concat![
+                "000000001F0AE1AC45FB0A51281FED31D539D874B03371D5434EE69C7621B729",
+                "ED7AEE323E53C6126965E348A0290FCB0D082D737C97BA987A385155BEE7079F",
+                "8665EEB269B687C31CA11815F4B8436A374AD8B83FE024778D4857517C5941DA",
+                "C70D778BCCEF36A81AED8DA0B819D2BD28BD8653E56A5D40903DF1A0ADE0B876"
+            ])
+        );
+
+        // Test that the RNG is in the same state
+        let mut state = [0u8; 16];
+        rng.fill_bytes(&mut state);
+
+        assert_eq!(
+            state,
+            [75, 121, 77, 111, 45, 9, 160, 230, 99, 38, 108, 225, 174, 126, 209, 8]
+        );
     }
 }
