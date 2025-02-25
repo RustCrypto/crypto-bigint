bae5a34da22f3df0ee5856a7a78158f0a0ec6119
Erik Takke <erik.takke@3milabs.tech>
2025-02-25 11:14:03 +0100

    Fix fmt

--- src/uint/rand.rs
+++ src/uint/rand.rs
@@ -149,8 +149,8 @@ pub(super) fn random_mod_core<T, R: RngCore + ?Sized>(
 mod tests {
     use crate::uint::rand::random_bits_core;
     use crate::{Limb, NonZero, RandomBits, RandomMod, Uint, U1024, U256};
-    use rand_chacha::{ChaCha20Core, ChaCha20Rng};
     use rand_chacha::ChaCha8Rng;
+    use rand_chacha::{ChaCha20Core, ChaCha20Rng};
     use rand_core::{RngCore, SeedableRng};
 
     #[test]
