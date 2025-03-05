d62e139e1d263df9ce312e846642509020ab2763
Bogdan Opanchuk <bogdan@opanchuk.net>
2025-03-05 10:33:24 -0800

    Use AMM in BoxedMontyForm::retrieve

--- src/modular/boxed_monty_form/mul.rs
+++ src/modular/boxed_monty_form/mul.rs
@@ -6,7 +6,7 @@
 //! Originally (c) 2014 The Rust Project Developers, dual licensed Apache 2.0+MIT.
 
 use super::{BoxedMontyForm, BoxedMontyParams};
-use crate::{BoxedUint, Limb, Square, SquareAssign, Word, Zero};
+use crate::{BoxedUint, ConstChoice, Limb, Square, SquareAssign, Word, Zero};
 use core::{
     borrow::Borrow,
     ops::{Mul, MulAssign},
@@ -133,6 +133,26 @@ impl<'a> MontyMultiplier<'a> {
         a.sub_assign_mod_with_carry(Limb::ZERO, self.modulus, self.modulus);
 
         debug_assert!(&*a < self.modulus);
+    }
+
+    /// Perform a Montgomery multiplication, assigning a fully reduced result to `a`.
+    pub(super) fn mul_by_one(&mut self, a: &BoxedUint) -> BoxedUint {
+        debug_assert_eq!(a.bits_precision(), self.modulus.bits_precision());
+
+        let mut ret = a.clone();
+
+        self.clear_product();
+        almost_montgomery_mul_by_one(
+            self.product.as_limbs_mut(),
+            a.as_limbs(),
+            self.modulus.as_limbs(),
+            self.mod_neg_inv,
+        );
+        ret.limbs
+            .copy_from_slice(&self.product.limbs[..a.limbs.len()]);
+        ret.sub_assign_mod_with_carry(Limb::ZERO, self.modulus, self.modulus);
+
+        ret
     }
 
     /// Perform a squaring using Montgomery multiplication, returning a fully reduced result.
@@ -276,6 +296,53 @@ fn almost_montgomery_mul(z: &mut [Limb], x: &[Limb], y: &[Limb], m: &[Limb], k: 
     let is_zero = c.is_zero();
     for (a, b) in lower.iter_mut().zip(upper.iter()) {
         a.conditional_assign(b, is_zero);
+    }
+}
+
+/// Same as `almost_montgomery_mul` with `y == 1`.
+///
+/// Used for retrieving from Montgomery form.
+fn almost_montgomery_mul_by_one(z: &mut [Limb], x: &[Limb], m: &[Limb], k: Limb) {
+    // This code assumes x, m are all the same length (required by addMulVVW and the for loop).
+    // It also assumes that x is already reduced mod m, or else the result will not be properly
+    // reduced.
+    let n = m.len();
+
+    // This preconditions check allows compiler to remove bound checks later in the code.
+    // `z.len() > n && z[n..].len() == n` is used intentionally instead of `z.len() == 2* n`
+    // since the latter prevents compiler from removing some bound checks.
+    let pre_cond = z.len() > n && z[n..].len() == n && x.len() == n && m.len() == n;
+    if !pre_cond {
+        panic!("Failed preconditions in montgomery_mul");
+    }
+
+    let mut c = ConstChoice::FALSE;
+
+    // The unrolled first iteration.
+    let c2 = add_mul_vvw(&mut z[0..n], x, Limb::ONE);
+    let t = z[0].wrapping_mul(k);
+    let c3 = add_mul_vvw(&mut z[0..n], m, t);
+    let cx = c2.wrapping_add(Limb(c.to_u8() as Word));
+    let cy = cx.wrapping_add(c3);
+    z[n] = cy;
+    c = ConstChoice::from_word_lt(cx.0, c2.0).or(ConstChoice::from_word_lt(cy.0, c3.0));
+
+    for i in 1..n {
+        let c2 = add_mul_vvw(&mut z[i..n + i], x, Limb::ZERO);
+        let t = z[i].wrapping_mul(k);
+        let c3 = add_mul_vvw(&mut z[i..n + i], m, t);
+        let cx = c2.wrapping_add(Limb(c.to_u8() as Word));
+        let cy = cx.wrapping_add(c3);
+        z[n + i] = cy;
+        c = ConstChoice::from_word_lt(cx.0, c2.0).or(ConstChoice::from_word_lt(cy.0, c3.0));
+    }
+
+    let (lower, upper) = z.split_at_mut(n);
+    sub_vv(lower, upper, m);
+
+    let is_zero = c.not();
+    for (a, b) in lower.iter_mut().zip(upper.iter()) {
+        *a = Limb::select(*a, *b, is_zero);
     }
 }
 
