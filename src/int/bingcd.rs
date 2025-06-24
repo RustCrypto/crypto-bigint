55e85f2d954c06af82f2717e2688b30e543c2fc4
Erik Takke <erik.takke@3milabs.tech>
2025-06-24 11:09:56 +0200

    Refactor `Int::bingcd` tests

--- src/int/bingcd.rs
+++ src/int/bingcd.rs
@@ -48,57 +48,54 @@ impl<const LIMBS: usize> Odd<Int<LIMBS>> {
 
 #[cfg(all(test, not(miri)))]
 mod tests {
-    use crate::{Gcd, I64, I128, I256, I512, I1024, I2048, I4096, Int, Uint};
 
-    fn int_bingcd_test<const LIMBS: usize>(lhs: Int<LIMBS>, rhs: Int<LIMBS>, target: Uint<LIMBS>)
-    where
-        Uint<LIMBS>: Gcd<Output = Uint<LIMBS>>,
-    {
-        assert_eq!(lhs.bingcd(&rhs), target);
-        assert_eq!(lhs.bingcd_vartime(&rhs), target);
-    }
+    mod bingcd {
+        use crate::{I64, I128, I256, I512, I1024, I2048, I4096, Int, Uint};
 
-    fn int_bingcd_tests<const LIMBS: usize>()
-    where
-        Uint<LIMBS>: Gcd<Output = Uint<LIMBS>>,
-    {
-        let abs_min = *Int::MIN.as_uint();
-        let max = *Int::MAX.as_uint();
-        int_bingcd_test(Int::MIN, Int::MIN, abs_min);
-        int_bingcd_test(Int::MIN, Int::MINUS_ONE, Uint::ONE);
-        int_bingcd_test(Int::MIN, Int::ZERO, abs_min);
-        int_bingcd_test(Int::MIN, Int::ONE, Uint::ONE);
-        int_bingcd_test(Int::MIN, Int::MAX, Uint::ONE);
-        int_bingcd_test(Int::MINUS_ONE, Int::MIN, Uint::ONE);
-        int_bingcd_test(Int::MINUS_ONE, Int::MINUS_ONE, Uint::ONE);
-        int_bingcd_test(Int::MINUS_ONE, Int::ZERO, Uint::ONE);
-        int_bingcd_test(Int::MINUS_ONE, Int::ONE, Uint::ONE);
-        int_bingcd_test(Int::MINUS_ONE, Int::MAX, Uint::ONE);
-        int_bingcd_test(Int::ZERO, Int::MIN, abs_min);
-        int_bingcd_test(Int::ZERO, Int::MINUS_ONE, Uint::ONE);
-        int_bingcd_test(Int::ZERO, Int::ZERO, Uint::ZERO);
-        int_bingcd_test(Int::ZERO, Int::ONE, Uint::ONE);
-        int_bingcd_test(Int::ZERO, Int::MAX, max);
-        int_bingcd_test(Int::ONE, Int::MIN, Uint::ONE);
-        int_bingcd_test(Int::ONE, Int::MINUS_ONE, Uint::ONE);
-        int_bingcd_test(Int::ONE, Int::ZERO, Uint::ONE);
-        int_bingcd_test(Int::ONE, Int::ONE, Uint::ONE);
-        int_bingcd_test(Int::ONE, Int::MAX, Uint::ONE);
-        int_bingcd_test(Int::MAX, Int::MIN, Uint::ONE);
-        int_bingcd_test(Int::MAX, Int::MINUS_ONE, Uint::ONE);
-        int_bingcd_test(Int::MAX, Int::ZERO, max);
-        int_bingcd_test(Int::MAX, Int::ONE, Uint::ONE);
-        int_bingcd_test(Int::MAX, Int::MAX, max);
-    }
+        fn test<const LIMBS: usize>(lhs: Int<LIMBS>, rhs: Int<LIMBS>, target: Uint<LIMBS>) {
+            assert_eq!(lhs.bingcd(&rhs), target);
+            assert_eq!(lhs.bingcd_vartime(&rhs), target);
+        }
 
-    #[test]
-    fn test_int_bingcd() {
-        int_bingcd_tests::<{ I64::LIMBS }>();
-        int_bingcd_tests::<{ I128::LIMBS }>();
-        int_bingcd_tests::<{ I256::LIMBS }>();
-        int_bingcd_tests::<{ I512::LIMBS }>();
-        int_bingcd_tests::<{ I1024::LIMBS }>();
-        int_bingcd_tests::<{ I2048::LIMBS }>();
-        int_bingcd_tests::<{ I4096::LIMBS }>();
+        fn run_tests<const LIMBS: usize>() {
+            let abs_min = *Int::MIN.as_uint();
+            let max = *Int::MAX.as_uint();
+            test(Int::<LIMBS>::MIN, Int::MIN, abs_min);
+            test(Int::<LIMBS>::MIN, Int::MINUS_ONE, Uint::ONE);
+            test(Int::<LIMBS>::MIN, Int::ZERO, abs_min);
+            test(Int::<LIMBS>::MIN, Int::ONE, Uint::ONE);
+            test(Int::<LIMBS>::MIN, Int::MAX, Uint::ONE);
+            test(Int::<LIMBS>::MINUS_ONE, Int::MIN, Uint::ONE);
+            test(Int::<LIMBS>::MINUS_ONE, Int::MINUS_ONE, Uint::ONE);
+            test(Int::<LIMBS>::MINUS_ONE, Int::ZERO, Uint::ONE);
+            test(Int::<LIMBS>::MINUS_ONE, Int::ONE, Uint::ONE);
+            test(Int::<LIMBS>::MINUS_ONE, Int::MAX, Uint::ONE);
+            test(Int::<LIMBS>::ZERO, Int::MIN, abs_min);
+            test(Int::<LIMBS>::ZERO, Int::MINUS_ONE, Uint::ONE);
+            test(Int::<LIMBS>::ZERO, Int::ZERO, Uint::ZERO);
+            test(Int::<LIMBS>::ZERO, Int::ONE, Uint::ONE);
+            test(Int::<LIMBS>::ZERO, Int::MAX, max);
+            test(Int::<LIMBS>::ONE, Int::MIN, Uint::ONE);
+            test(Int::<LIMBS>::ONE, Int::MINUS_ONE, Uint::ONE);
+            test(Int::<LIMBS>::ONE, Int::ZERO, Uint::ONE);
+            test(Int::<LIMBS>::ONE, Int::ONE, Uint::ONE);
+            test(Int::<LIMBS>::ONE, Int::MAX, Uint::ONE);
+            test(Int::<LIMBS>::MAX, Int::MIN, Uint::ONE);
+            test(Int::<LIMBS>::MAX, Int::MINUS_ONE, Uint::ONE);
+            test(Int::<LIMBS>::MAX, Int::ZERO, max);
+            test(Int::<LIMBS>::MAX, Int::ONE, Uint::ONE);
+            test(Int::<LIMBS>::MAX, Int::MAX, max);
+        }
+
+        #[test]
+        fn bingcd() {
+            run_tests::<{ I64::LIMBS }>();
+            run_tests::<{ I128::LIMBS }>();
+            run_tests::<{ I256::LIMBS }>();
+            run_tests::<{ I512::LIMBS }>();
+            run_tests::<{ I1024::LIMBS }>();
+            run_tests::<{ I2048::LIMBS }>();
+            run_tests::<{ I4096::LIMBS }>();
+        }
     }
 }
