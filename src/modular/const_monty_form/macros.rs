RustCrypto/crypto-bigint

commit 73111915db06477c08ac0f6c74e8dae3dd3716f1

Rename `impl_modulus!` => `const_monty_params!` (#861)
The old name is preserved with a deprecation.

The new name matches the `ConstMontyParams` trait name, and also matches
the `const_monty_form!` macro.

src/modular/const_monty_form/macros.rs
@@ -1,19 +1,30 @@
-//! Macro support.
+//! [`ConstMontyForm`]/[`ConstMontyParams`] support macros.
 
-/// Implements a modulus with the given name, type, and value, in that specific order. Please
-/// `use crypto_bigint::traits::Encoding` to make this work.
+#[cfg(doc)]
+use crate::modular::{ConstMontyForm, ConstMontyParams};
+
+/// Create a type representing a modulus which impls the [`ConstMontyParams`] trait with the given
+/// name, type, value (in big endian hex), and optional documentation string.
 ///
-/// For example,
-/// `impl_modulus!(MyModulus, U256, "73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001");`
-/// implements a 256-bit modulus named `MyModulus`.
+/// # Usage
+///
+/// ```
+/// use crypto_bigint::{U256, const_monty_params};
+///
+/// const_monty_params!(
+///     MyModulus,
+///     U256,
+///     "73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001",
+///     "Docs for my modulus"
+/// );
+/// ```
 ///
 /// The modulus _must_ be odd, or this will panic.
-// TODO: Use `adt_const_params` once stabilized to make a `ConstMontyForm` generic around a modulus rather
-// than having to implement a ZST + trait
+// TODO: use `adt_const_params` when stable to make a `ConstMontyForm` generic around a modulus
 #[macro_export]
-macro_rules! impl_modulus {
+macro_rules! const_monty_params {
     ($name:ident, $uint_type:ty, $value:expr) => {
-        $crate::impl_modulus!(
+        $crate::const_monty_params!(
             $name,
             $uint_type,
             $value,
@@ -72,16 +83,44 @@ macro_rules! impl_modulus {
     };
 }
 
-/// Creates a `ConstMontyForm` with the given value for a specific modulus.
+/// Creates a [`ConstMontyForm`] with the given value for a specific modulus, i.e. a type which
+/// impls [`ConstMontyParams`].
 ///
-/// For example, `const_monty_form!(U256::from(105u64), MyModulus);`
-/// creates a `ConstMontyForm` for 105 mod `MyModulus`.
+/// # Usage
 ///
-/// The modulus _must_ be odd, or this will panic.
+/// ```
+/// use crypto_bigint::{U256, modular::ConstMontyParams, const_monty_form};
+/// #
+/// # crypto_bigint::const_monty_params!(
+/// #    MyModulus,
+/// #    U256,
+/// #    "73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001",
+/// #    "Docs for my modulus"
+/// # );
+///
+/// const_monty_form!(U256::from(105u64), MyModulus);
+/// ```
 #[macro_export]
 macro_rules! const_monty_form {
-    ($variable:ident, $modulus:ident) => {
-        $crate::modular::ConstMontyForm::<$modulus, { $modulus::LIMBS }>::new(&$variable)
+    ($value:expr, $modulus:ident) => {
+        $crate::modular::ConstMontyForm::<$modulus, { $modulus::LIMBS }>::new(&$value)
+    };
+}
+
+/// Deprecated legacy macro which has been replaced by [`const_monty_form!`].
+#[deprecated(since = "0.7.0", note = "use `const_monty_params!` instead")]
+#[macro_export]
+macro_rules! impl_modulus {
+    ($name:ident, $uint_type:ty, $value:expr) => {
+        $crate:const_monty_params!(
+            $name,
+            $uint_type,
+            $value,
+            "Modulus which impls `ConstMontyParams`"
+        );
+    };
+    ($name:ident, $uint_type:ty, $value:expr, $doc:expr) => {
+        $crate::const_monty_params!($name, $uint_type, $value, $doc);
     };
 }
 
@@ -92,7 +131,7 @@ mod tests {
 
     #[test]
     fn new_params_with_valid_modulus() {
-        impl_modulus!(Mod, U64, "0000000000000003");
+        const_monty_params!(Mod, U64, "0000000000000003");
 
         assert_eq!(Mod::MOD_LEADING_ZEROS, core::cmp::min(Limb::BITS - 1, 62));
     }

benches/const_monty.rs
@@ -1,7 +1,7 @@
 use criterion::{
     BatchSize, BenchmarkGroup, Criterion, criterion_group, criterion_main, measurement::Measurement,
 };
-use crypto_bigint::{Random, RandomMod, U256, impl_modulus, modular::ConstMontyParams};
+use crypto_bigint::{Random, RandomMod, U256, const_monty_params, modular::ConstMontyParams};
 use rand_chacha::ChaChaRng;
 use rand_core::SeedableRng;
 use std::hint::black_box;
@@ -9,7 +9,7 @@ use std::hint::black_box;
 #[cfg(feature = "alloc")]
 use crypto_bigint::MultiExponentiate;
 
-impl_modulus!(
+const_monty_params!(
     Modulus,
     U256,
     "7fffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551"

src/modular.rs
@@ -57,14 +57,14 @@ pub trait Retrieve {
 #[cfg(test)]
 mod tests {
     use crate::{
-        NonZero, U64, U256, Uint, const_monty_form, impl_modulus,
+        NonZero, U64, U256, Uint, const_monty_form, const_monty_params,
         modular::{
             const_monty_form::{ConstMontyForm, ConstMontyParams},
             reduction::montgomery_reduction,
         },
     };
 
-    impl_modulus!(
+    const_monty_params!(
         Modulus1,
         U256,
         "73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001"
@@ -86,7 +86,7 @@ mod tests {
         );
     }
 
-    impl_modulus!(
+    const_monty_params!(
         Modulus2,
         U256,
         "ffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551"

src/modular/const_monty_form.rs
@@ -28,12 +28,11 @@ use {
 #[macro_use]
 mod macros;
 
-/// The parameters to efficiently go to and from the Montgomery form for a given odd modulus.
+/// Trait representing a modulus and its associated constants for converting in and out of
+/// Montgomery form.
 ///
-/// An easy way to generate these parameters is using the [`impl_modulus!`][`crate::impl_modulus`]
-/// macro. These parameters are constant, so they cannot be set at runtime.
-///
-/// Unfortunately, `LIMBS` must be generic for now until const generics are stabilized.
+/// To define a type which impls this trait, use the
+/// [`const_monty_params!`][`crate::const_monty_params`] macro.
 pub trait ConstMontyParams<const LIMBS: usize>:
     Copy + Debug + Default + Eq + Send + Sync + 'static
 {

src/modular/const_monty_form/add.rs
@@ -83,10 +83,10 @@ impl<MOD: ConstMontyParams<LIMBS>, const LIMBS: usize> AddAssign<Self>
 #[cfg(test)]
 mod tests {
     use crate::{
-        U256, const_monty_form, impl_modulus, modular::const_monty_form::ConstMontyParams,
+        U256, const_monty_form, const_monty_params, modular::const_monty_form::ConstMontyParams,
     };
 
-    impl_modulus!(
+    const_monty_params!(
         Modulus,
         U256,
         "ffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551"

src/modular/const_monty_form/invert.rs
@@ -223,9 +223,9 @@ where
 #[cfg(test)]
 mod tests {
     use super::ConstMontyParams;
-    use crate::{U256, const_monty_form, impl_modulus};
+    use crate::{U256, const_monty_form, const_monty_params};
 
-    impl_modulus!(
+    const_monty_params!(
         Modulus,
         U256,
         "15477BCCEFE197328255BFA79A1217899016D927EF460F4FF404029D24FA4409"

src/modular/const_monty_form/lincomb.rs
@@ -25,9 +25,9 @@ mod tests {
     #[test]
     fn lincomb_expected() {
         use super::{ConstMontyForm, ConstMontyParams};
-        use crate::{RandomMod, U256, impl_modulus};
+        use crate::{RandomMod, U256, const_monty_params};
         use rand_core::SeedableRng;
-        impl_modulus!(
+        const_monty_params!(
             P,
             U256,
             "7fffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551"

src/modular/const_monty_form/neg.rs
@@ -30,10 +30,10 @@ impl<MOD: ConstMontyParams<LIMBS>, const LIMBS: usize> Neg for &ConstMontyForm<M
 #[cfg(test)]
 mod tests {
     use crate::{
-        U256, const_monty_form, impl_modulus, modular::const_monty_form::ConstMontyParams,
+        U256, const_monty_form, const_monty_params, modular::const_monty_form::ConstMontyParams,
     };
 
-    impl_modulus!(
+    const_monty_params!(
         Modulus,
         U256,
         "15477BCCEFE197328255BFA79A1217899016D927EF460F4FF404029D24FA4409"

commit 8d37fdfd0438083360eaf279a6c1e06079d79712

Fix deprecated `impl_modulus!` macro
Also adds a test it still works. It was broken in #861 (missing colon)
when it was changed to invoke the new `const_monty_params!` macro.

Closes #865

src/modular/const_monty_form/macros.rs
@@ -112,7 +112,7 @@ macro_rules! const_monty_form {
 #[macro_export]
 macro_rules! impl_modulus {
     ($name:ident, $uint_type:ty, $value:expr) => {
-        $crate:const_monty_params!(
+        $crate::const_monty_params!(
             $name,
             $uint_type,
             $value,
@@ -132,7 +132,15 @@ mod tests {
     #[test]
     fn new_params_with_valid_modulus() {
         const_monty_params!(Mod, U64, "0000000000000003");
+        assert_eq!(Mod::MOD_LEADING_ZEROS, core::cmp::min(Limb::BITS - 1, 62));
+    }
 
+    /// Make sure the deprecated macro still works
+    // TODO(tarcieri): remove this in the next breaking release
+    #[test]
+    #[allow(deprecated)]
+    fn impl_modulus_with_valid_modulus() {
+        impl_modulus!(Mod, U64, "0000000000000003");
         assert_eq!(Mod::MOD_LEADING_ZEROS, core::cmp::min(Limb::BITS - 1, 62));
     }
 }