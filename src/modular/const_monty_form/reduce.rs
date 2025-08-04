9a09377161f3b2c48b071055b0e162e9ea96db22
Tony Arcieri <bascule@gmail.com>
2025-08-04 11:29:26 -0600

    Have `Reduce` borrow its input (#906)
    Impls of `Reduce` only subsequently call functions which borrow, and
    have no need to actually take ownership of their inputs.
    
    Per the Rust API guidelines, in such a case a function should borrow its
    input:
    
    
    https://rust-lang.github.io/api-guidelines/flexibility.html?highlight=clone#caller-decides-where-to-copy-and-place-data-c-caller-control

--- src/modular/const_monty_form/reduce.rs
+++ src/modular/const_monty_form/reduce.rs
@@ -1,17 +1,7 @@
+use super::ConstMontyParams;
 use crate::{Reduce, Uint, modular::ConstMontyForm};
 
-use super::ConstMontyParams;
-
 impl<const LIMBS: usize, MOD> Reduce<Uint<LIMBS>> for ConstMontyForm<MOD, LIMBS>
-where
-    MOD: ConstMontyParams<LIMBS>,
-{
-    fn reduce(value: Uint<LIMBS>) -> Self {
-        Self::new(&value)
-    }
-}
-
-impl<const LIMBS: usize, MOD> Reduce<&Uint<LIMBS>> for ConstMontyForm<MOD, LIMBS>
 where
     MOD: ConstMontyParams<LIMBS>,
 {
