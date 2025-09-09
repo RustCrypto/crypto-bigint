a04647bf4f3be019299808af00780a94835ddd82
Andrew Whitehead <cywolf@gmail.com>
2025-09-09 11:42:11 -0600

    change debug assert to assert
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/uint/ref_type/sub.rs
+++ src/uint/ref_type/sub.rs
@@ -11,7 +11,7 @@ impl UintRef {
     /// Perform an in-place borrowing subtraction of another limb slice, returning the borrowed limb value.
     #[inline]
     pub const fn borrowing_sub_assign_slice(&mut self, rhs: &[Limb], mut borrow: Limb) -> Limb {
-        debug_assert!(self.0.len() == rhs.len(), "length mismatch");
+        assert!(self.0.len() == rhs.len(), "length mismatch");
         let mut i = 0;
         while i < self.0.len() {
             (self.0[i], borrow) = self.0[i].borrowing_sub(rhs[i], borrow);
