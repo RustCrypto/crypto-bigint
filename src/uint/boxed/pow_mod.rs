4e920d6a01d829075a132f7c531613404d9c1c4f
Tony Arcieri <bascule@gmail.com>
2026-01-12 09:14:37 -0700

    Add `BoxedUint::pow_mod` (#1084)
    Simplified modular exponentiation interface for cases where it doesn't
    make sense to create persistent Montgomery form values (i.e.
    `BoxedMontyForm`)

---
+++ src/uint/boxed/pow_mod.rs
@@ -0,0 +1,17 @@
+//! [`BoxedUint`] modular exponentiation operations.
+
+use crate::{
+    BoxedUint, Odd,
+    modular::{BoxedMontyForm, BoxedMontyParams},
+};
+
+impl BoxedUint {
+    /// Computes `self ^ rhs mod p` for odd `p`.
+    pub fn pow_mod(&self, rhs: &BoxedUint, p: &Odd<BoxedUint>) -> BoxedUint {
+        BoxedMontyForm::new(self.clone(), BoxedMontyParams::new(p.clone()))
+            .pow(rhs)
+            .retrieve()
+    }
+}
+
+// NOTE: tested via proptests in `tests/boxed_uint.rs`
