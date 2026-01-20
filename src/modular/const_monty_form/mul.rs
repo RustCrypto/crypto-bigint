abb86230639604373e194e32f1c79e006012a938
Andrew Whitehead <cywolf@gmail.com>
2026-01-19 17:18:14 -0700

    Add `square_repeat_vartime` method for `MontyForm`, `ConstMontyForm` (#1122)
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/modular/const_monty_form/mul.rs
+++ src/modular/const_monty_form/mul.rs
@@ -6,7 +6,7 @@ use core::{
 };
 
 use crate::{
-    modular::mul::{mul_montgomery_form, square_montgomery_form},
+    modular::mul::{mul_montgomery_form, square_montgomery_form, square_repeat_montgomery_form},
     traits::Square,
 };
 
@@ -33,6 +33,22 @@ impl<MOD: ConstMontyParams<LIMBS>, const LIMBS: usize> ConstMontyForm<MOD, LIMBS
         Self {
             montgomery_form: square_montgomery_form(
                 &self.montgomery_form,
+                &MOD::PARAMS.modulus,
+                MOD::PARAMS.mod_neg_inv(),
+            ),
+            phantom: PhantomData,
+        }
+    }
+
+    /// Computes the reduced product `self^2n`.
+    ///
+    /// This method is variable time in `n` only.
+    #[must_use]
+    pub const fn square_repeat_vartime(&self, n: u32) -> Self {
+        Self {
+            montgomery_form: square_repeat_montgomery_form::<LIMBS>(
+                &self.montgomery_form,
+                n,
                 &MOD::PARAMS.modulus,
                 MOD::PARAMS.mod_neg_inv(),
             ),
@@ -98,5 +114,64 @@ impl<MOD: ConstMontyParams<LIMBS>, const LIMBS: usize> MulAssign<Self>
 impl<MOD: ConstMontyParams<LIMBS>, const LIMBS: usize> Square for ConstMontyForm<MOD, LIMBS> {
     fn square(&self) -> Self {
         ConstMontyForm::square(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        U256, const_monty_form, const_monty_params,
+        modular::const_monty_form::{ConstMontyForm, ConstMontyParams},
+    };
+
+    const_monty_params!(
+        Modulus,
+        U256,
+        "15477BCCEFE197328255BFA79A1217899016D927EF460F4FF404029D24FA4409"
+    );
+
+    const_monty_form!(Fe, Modulus);
+
+    const N: U256 =
+        U256::from_be_hex("14117F1273373C26C700D076B3F780074D03339F56DD0EFB60E7F58441FD3685");
+    const N_MOD: ConstMontyForm<Modulus, { U256::LIMBS }> = ConstMontyForm::new(&N);
+
+    #[test]
+    fn test_mul_zero() {
+        let res = N_MOD.mul(&Fe::ZERO);
+        let expected = U256::ZERO;
+        assert_eq!(res.retrieve(), expected);
+    }
+
+    #[test]
+    fn test_mul_one() {
+        let res = N_MOD.mul(&Fe::ONE);
+        assert_eq!(res.retrieve(), N);
+    }
+
+    #[test]
+    fn test_mul_eq_add() {
+        let res = N_MOD.mul(&Fe::new(&U256::from(2u8)));
+        assert_eq!(res, N_MOD.add(&N_MOD));
+    }
+
+    #[test]
+    fn test_square_eq_mul() {
+        let res = N_MOD.square();
+        let expected = N_MOD.mul(&N_MOD);
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn test_square_repeat() {
+        let res = N_MOD.square_repeat_vartime(0);
+        assert_eq!(res, N_MOD);
+
+        let res = N_MOD.square_repeat_vartime(1);
+        assert_eq!(res, N_MOD.square());
+
+        let res = N_MOD.square_repeat_vartime(5);
+        let expected = N_MOD.square().square().square().square().square();
+        assert_eq!(res, expected);
     }
 }
--- src/modular/monty_form/mul.rs
+++ src/modular/monty_form/mul.rs
@@ -5,7 +5,7 @@ use crate::{
     MontyMultiplier, Square, SquareAssign,
     modular::{
         MontyParams,
-        mul::{mul_montgomery_form, square_montgomery_form},
+        mul::{mul_montgomery_form, square_montgomery_form, square_repeat_montgomery_form},
     },
 };
 use core::ops::{Mul, MulAssign};
@@ -31,6 +31,22 @@ impl<const LIMBS: usize> MontyForm<LIMBS> {
         Self {
             montgomery_form: square_montgomery_form(
                 &self.montgomery_form,
+                &self.params.modulus,
+                self.params.mod_neg_inv(),
+            ),
+            params: self.params,
+        }
+    }
+
+    /// Computes the reduced product `self^2n`.
+    ///
+    /// This method is variable time in `n` only.
+    #[must_use]
+    pub const fn square_repeat_vartime(&self, n: u32) -> Self {
+        Self {
+            montgomery_form: square_repeat_montgomery_form::<LIMBS>(
+                &self.montgomery_form,
+                n,
                 &self.params.modulus,
                 self.params.mod_neg_inv(),
             ),
@@ -122,5 +138,60 @@ impl<'a, const LIMBS: usize> MontyMultiplier<'a> for DynMontyMultiplier<'a, LIMB
         let product =
             square_montgomery_form(&lhs.montgomery_form, &self.0.modulus, self.0.mod_neg_inv());
         lhs.montgomery_form = product;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        Odd, U256,
+        modular::{MontyForm, MontyParams},
+    };
+
+    const PARAMS: MontyParams<{ U256::LIMBS }> =
+        MontyParams::new_vartime(Odd::<U256>::from_be_hex(
+            "ffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551",
+        ));
+    const N: U256 =
+        U256::from_be_hex("14117F1273373C26C700D076B3F780074D03339F56DD0EFB60E7F58441FD3685");
+    const N_MOD: MontyForm<{ U256::LIMBS }> = MontyForm::new(&N, &PARAMS);
+
+    #[test]
+    fn test_mul_zero() {
+        let res = N_MOD.mul(&MontyForm::zero(PARAMS));
+        let expected = U256::ZERO;
+        assert_eq!(res.retrieve(), expected);
+    }
+
+    #[test]
+    fn test_mul_one() {
+        let res = N_MOD.mul(&MontyForm::one(PARAMS));
+        assert_eq!(res.retrieve(), N);
+    }
+
+    #[test]
+    fn test_mul_eq_add() {
+        let res = N_MOD.mul(&MontyForm::new(&U256::from(2u8), &PARAMS));
+        assert_eq!(res, N_MOD.add(&N_MOD));
+    }
+
+    #[test]
+    fn test_square_eq_mul() {
+        let res = N_MOD.square();
+        let expected = N_MOD.mul(&N_MOD);
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn test_square_repeat() {
+        let res = N_MOD.square_repeat_vartime(0);
+        assert_eq!(res, N_MOD);
+
+        let res = N_MOD.square_repeat_vartime(1);
+        assert_eq!(res, N_MOD.square());
+
+        let res = N_MOD.square_repeat_vartime(5);
+        let expected = N_MOD.square().square().square().square().square();
+        assert_eq!(res, expected);
     }
 }
