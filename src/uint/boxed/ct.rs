d3ec7f05ff280d7b8916217983839f6fe473c330
Andrew Whitehead <cywolf@gmail.com>
2026-02-02 14:53:06 -0700

    use UintRef methods to compare Int, Uint, and BoxedUint
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/uint/boxed/ct.rs
+++ src/uint/boxed/ct.rs
@@ -1,33 +1,22 @@
 //! Constant-time support: impls of `Ct*` traits.
 
 use super::BoxedUint;
-use crate::{Choice, CtAssign, CtEq, CtGt, CtLt, CtNeg, CtSelect, Limb, word};
-use core::cmp::max;
+use crate::{Choice, CtAssign, CtEq, CtGt, CtLt, CtNeg, CtSelect, Limb, UintRef};
 use ctutils::{CtAssignSlice, CtEqSlice};
 
 impl CtAssign for BoxedUint {
     #[inline]
     fn ct_assign(&mut self, other: &Self, choice: Choice) {
-        debug_assert_eq!(self.bits_precision(), other.bits_precision());
+        assert_eq!(self.bits_precision(), other.bits_precision());
         self.limbs.ct_assign(&other.limbs, choice);
     }
 }
 impl CtAssignSlice for BoxedUint {}
 
-// Note: special impl with implicit widening
-impl CtEq for BoxedUint {
+impl<Rhs: AsRef<UintRef> + ?Sized> CtEq<Rhs> for BoxedUint {
     #[inline]
-    fn ct_eq(&self, other: &Self) -> Choice {
-        let limbs = max(self.nlimbs(), other.nlimbs());
-        let mut ret = Choice::TRUE;
-
-        for i in 0..limbs {
-            let a = self.limbs.get(i).unwrap_or(&Limb::ZERO);
-            let b = other.limbs.get(i).unwrap_or(&Limb::ZERO);
-            ret &= a.ct_eq(b);
-        }
-
-        ret
+    fn ct_eq(&self, other: &Rhs) -> Choice {
+        self.as_uint_ref().ct_eq(other)
     }
 }
 impl CtEqSlice for BoxedUint {}
@@ -35,35 +24,33 @@ impl CtEqSlice for BoxedUint {}
 impl CtGt for BoxedUint {
     #[inline]
     fn ct_gt(&self, other: &Self) -> Choice {
-        let (_, borrow) = other.borrowing_sub(self, Limb::ZERO);
-        word::choice_from_mask(borrow.0)
+        UintRef::lt(other.as_uint_ref(), self.as_uint_ref())
     }
 }
 
 impl CtLt for BoxedUint {
     #[inline]
     fn ct_lt(&self, other: &Self) -> Choice {
-        let (_, borrow) = self.borrowing_sub(other, Limb::ZERO);
-        word::choice_from_mask(borrow.0)
+        UintRef::lt(self.as_uint_ref(), other.as_uint_ref())
     }
 }
 
 impl CtNeg for BoxedUint {
     fn ct_neg(&self, choice: Choice) -> Self {
-        let self_neg = self.wrapping_neg();
-        self.ct_select(&self_neg, choice)
+        let mut neg = self.clone();
+        neg.conditional_wrapping_neg_assign(choice);
+        neg
     }
 
     fn ct_neg_assign(&mut self, choice: Choice) {
-        let self_neg = self.wrapping_neg();
-        self.ct_assign(&self_neg, choice);
+        self.conditional_wrapping_neg_assign(choice);
     }
 }
 
 impl CtSelect for BoxedUint {
     #[inline]
     fn ct_select(&self, other: &Self, choice: Choice) -> Self {
-        debug_assert_eq!(self.bits_precision(), other.bits_precision());
+        assert_eq!(self.bits_precision(), other.bits_precision());
         let mut limbs = vec![Limb::ZERO; self.nlimbs()].into_boxed_slice();
 
         for i in 0..self.nlimbs() {
@@ -75,7 +62,7 @@ impl CtSelect for BoxedUint {
 
     #[inline]
     fn ct_swap(&mut self, other: &mut Self, choice: Choice) {
-        debug_assert_eq!(self.bits_precision(), other.bits_precision());
+        assert_eq!(self.bits_precision(), other.bits_precision());
 
         for i in 0..self.nlimbs() {
             self.limbs[i].ct_swap(&mut other.limbs[i], choice);
