db4dbba6de838bbd94a3c4362c2ec523e9da8359
Andrew Whitehead <cywolf@gmail.com>
2026-02-01 17:35:19 -0700

    test trait implementations for Checked
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/checked.rs
+++ src/checked.rs
@@ -18,6 +18,15 @@ impl<T> Checked<T> {
     pub const fn new(val: T) -> Self {
         Self(CtOption::some(val))
     }
+
+    /// Create a new checked arithmetic wrapper for the given value.
+    #[cfg(test)]
+    pub(crate) fn none() -> Self
+    where
+        T: Default,
+    {
+        Self(CtOption::none())
+    }
 }
 
 impl<T> Add<Self> for Checked<T>
@@ -365,5 +374,191 @@ where
     #[inline]
     fn ct_eq(&self, rhs: &Self) -> subtle::Choice {
         CtEq::ct_eq(self, rhs).into()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Checked;
+    use crate::{
+        CheckedAdd, CheckedDiv, CheckedMul, CheckedSub, Choice, CtEq, CtOption, CtSelect, Limb,
+    };
+
+    // FIXME Could use itertools combinations or an equivalent iterator
+    const INPUTS: &[(Limb, Limb)] = &[
+        (Limb::ZERO, Limb::ZERO),
+        (Limb::ZERO, Limb::ONE),
+        (Limb::ZERO, Limb::MAX),
+        (Limb::ONE, Limb::ZERO),
+        (Limb::ONE, Limb::ONE),
+        (Limb::ONE, Limb::MAX),
+        (Limb::MAX, Limb::ZERO),
+        (Limb::MAX, Limb::ONE),
+        (Limb::MAX, Limb::MAX),
+    ];
+
+    #[test]
+    fn checked_new() {
+        assert_eq!(
+            Checked::<Limb>::default().0.into_option(),
+            Some(Limb::default())
+        );
+        for (a, _b) in INPUTS {
+            assert_eq!(Checked::<Limb>::new(*a).0.into_option(), Some(*a));
+        }
+    }
+
+    #[test]
+    fn checked_eq_select() {
+        let pairs: &[(Checked<Limb>, Checked<Limb>, bool)] = &[
+            (Checked::none(), Checked::none(), true),
+            (Checked::none(), Checked::new(Limb::ZERO), false),
+            (Checked::new(Limb::ZERO), Checked::none(), false),
+            (Checked::new(Limb::ZERO), Checked::new(Limb::ZERO), true),
+            (Checked::new(Limb::ZERO), Checked::new(Limb::ONE), false),
+        ];
+
+        for (a, b, eq) in pairs {
+            assert_eq!(a.ct_eq(b).to_bool(), *eq);
+            assert!(a.ct_select(b, Choice::FALSE).ct_eq(a).to_bool());
+            assert!(a.ct_select(b, Choice::TRUE).ct_eq(b).to_bool());
+            #[cfg(feature = "subtle")]
+            assert_eq!(bool::from(subtle::ConstantTimeEq::ct_eq(a, b)), *eq);
+            #[cfg(feature = "subtle")]
+            assert!(
+                subtle::ConditionallySelectable::conditional_select(
+                    a,
+                    b,
+                    subtle::Choice::from(0u8)
+                )
+                .ct_eq(a)
+                .to_bool()
+            );
+            #[cfg(feature = "subtle")]
+            assert!(
+                subtle::ConditionallySelectable::conditional_select(
+                    a,
+                    b,
+                    subtle::Choice::from(1u8)
+                )
+                .ct_eq(b)
+                .to_bool()
+            );
+        }
+    }
+
+    #[test]
+    fn checked_from() {
+        assert_eq!(Checked::<Limb>(CtOption::none()).0.into_option(), None);
+        assert_eq!(
+            Checked::<Limb>::default().0.into_option(),
+            Checked::<Limb>::from(CtOption::<Limb>::some(Limb::default()))
+                .0
+                .into_option(),
+        );
+        assert_eq!(
+            Checked::<Limb>::default().0.into_option(),
+            Into::<Option<Limb>>::into(Checked::<Limb>::default())
+        );
+        assert_eq!(
+            Checked::<Limb>::default().0.into_option(),
+            Into::<CtOption<Limb>>::into(Checked::<Limb>::default()).into_option()
+        );
+    }
+
+    #[allow(clippy::op_ref)]
+    #[test]
+    fn checked_add() {
+        for (a, b) in INPUTS {
+            let expect = a.checked_add(b).into_option();
+            assert_eq!(
+                (Checked::new(*a) + Checked::new(*b)).0.into_option(),
+                expect
+            );
+            assert_eq!(
+                (&Checked::new(*a) + Checked::new(*b)).0.into_option(),
+                expect
+            );
+            assert_eq!(
+                (Checked::new(*a) + &Checked::new(*b)).0.into_option(),
+                expect
+            );
+            assert_eq!(
+                (&Checked::new(*a) + &Checked::new(*b)).0.into_option(),
+                expect
+            );
+        }
+    }
+
+    #[allow(clippy::op_ref)]
+    #[test]
+    fn checked_sub() {
+        for (a, b) in INPUTS {
+            let expect = a.checked_sub(b).into_option();
+            assert_eq!(
+                (Checked::new(*a) - Checked::new(*b)).0.into_option(),
+                expect
+            );
+            assert_eq!(
+                (&Checked::new(*a) - Checked::new(*b)).0.into_option(),
+                expect
+            );
+            assert_eq!(
+                (Checked::new(*a) - &Checked::new(*b)).0.into_option(),
+                expect
+            );
+            assert_eq!(
+                (&Checked::new(*a) - &Checked::new(*b)).0.into_option(),
+                expect
+            );
+        }
+    }
+
+    #[allow(clippy::op_ref)]
+    #[test]
+    fn checked_mul() {
+        for (a, b) in INPUTS {
+            let expect = a.checked_mul(b).into_option();
+            assert_eq!(
+                (Checked::new(*a) * Checked::new(*b)).0.into_option(),
+                expect
+            );
+            assert_eq!(
+                (&Checked::new(*a) * Checked::new(*b)).0.into_option(),
+                expect
+            );
+            assert_eq!(
+                (Checked::new(*a) * &Checked::new(*b)).0.into_option(),
+                expect
+            );
+            assert_eq!(
+                (&Checked::new(*a) * &Checked::new(*b)).0.into_option(),
+                expect
+            );
+        }
+    }
+
+    #[allow(clippy::op_ref)]
+    #[test]
+    fn checked_div() {
+        for (a, b) in INPUTS {
+            let expect = a.checked_div(b).into_option();
+            assert_eq!(
+                (Checked::new(*a) / Checked::new(*b)).0.into_option(),
+                expect
+            );
+            assert_eq!(
+                (&Checked::new(*a) / Checked::new(*b)).0.into_option(),
+                expect
+            );
+            assert_eq!(
+                (Checked::new(*a) / &Checked::new(*b)).0.into_option(),
+                expect
+            );
+            assert_eq!(
+                (&Checked::new(*a) / &Checked::new(*b)).0.into_option(),
+                expect
+            );
+        }
     }
 }
