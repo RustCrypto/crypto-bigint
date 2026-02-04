f086ea33d2b6e9b7abcc15ba4ede4cac61c22e06
Andrew Whitehead <cywolf@gmail.com>
2026-02-04 14:51:20 -0700

    improve tests and reduce allocations for BoxedUint::mul_mod_special, sub_mod_special
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/uint/boxed/mul_mod.rs
+++ src/uint/boxed/mul_mod.rs
@@ -1,8 +1,6 @@
 //! [`BoxedUint`] modular multiplication operations.
 
-use crate::{
-    BoxedUint, Limb, MulMod, NonZero, SquareMod, U64, U128, WideWord, Word, div_limb::mul_rem,
-};
+use crate::{BoxedUint, Limb, MulMod, NonZero, SquareMod, UintRef, div_limb::mul_rem};
 
 impl BoxedUint {
     /// Computes `self * rhs mod p` for non-zero `p`.
@@ -21,34 +19,30 @@ impl BoxedUint {
     pub fn mul_mod_special(&self, rhs: &Self, c: Limb) -> Self {
         debug_assert_eq!(self.bits_precision(), rhs.bits_precision());
 
-        // We implicitly assume `LIMBS > 0`, because `Uint<0>` doesn't compile.
-        // Still the case `LIMBS == 1` needs special handling.
         if self.nlimbs() == 1 {
             let reduced = mul_rem(
                 self.limbs[0],
                 rhs.limbs[0],
-                NonZero::<Limb>::new_unwrap(Limb(Word::MIN.wrapping_sub(c.0))),
+                NonZero::<Limb>::new_unwrap(Limb::ZERO.wrapping_sub(c)),
             );
             return Self::from(reduced);
         }
 
         let product = self.mul(rhs);
-        let (lo_words, hi_words) = product.limbs.split_at(self.nlimbs());
-        let lo = BoxedUint::from(lo_words);
-        let hi = BoxedUint::from(hi_words);
+        let (lo, hi) = product.as_uint_ref().split_at(self.nlimbs());
 
         // Now use Algorithm 14.47 for the reduction
-        let (lo, carry) = mac_by_limb(&lo, &hi, c, Limb::ZERO);
+        let (mut lo, carry) = mac_by_limb(lo, hi, c, Limb::ZERO);
 
-        let (lo, carry) = {
-            let rhs = WideWord::from(carry.0 + 1) * WideWord::from(c.0);
-            lo.carrying_add(U128::from(rhs), Limb::ZERO)
+        let carry = {
+            let rhs = carry.wrapping_add(Limb::ONE).widening_mul(c);
+            lo.carrying_add_assign(UintRef::new(&[rhs.0, rhs.1]), Limb::ZERO)
         };
 
-        let (lo, _) = {
-            let rhs = carry.0.wrapping_sub(1) & c.0;
-            lo.borrowing_sub(U64::from(rhs), Limb::ZERO)
-        };
+        {
+            let rhs = carry.wrapping_sub(Limb::ONE) & c;
+            lo.borrowing_sub_assign(rhs, Limb::ZERO);
+        }
 
         lo
     }
@@ -83,85 +77,72 @@ impl SquareMod for BoxedUint {
 }
 
 /// Computes `a + (b * c) + carry`, returning the result along with the new carry.
-fn mac_by_limb(a: &BoxedUint, b: &BoxedUint, c: Limb, carry: Limb) -> (BoxedUint, Limb) {
-    let mut a = a.clone();
+fn mac_by_limb(a: &UintRef, b: &UintRef, c: Limb, carry: Limb) -> (BoxedUint, Limb) {
+    let mut res = BoxedUint::zero_with_precision(a.bits_precision());
     let mut carry = carry;
 
     for i in 0..a.nlimbs() {
-        let (n, c) = b.limbs[i].carrying_mul_add(c, a.limbs[i], carry);
-        a.limbs[i] = n;
-        carry = c;
+        (res.limbs[i], carry) = b.limbs[i].carrying_mul_add(c, a.limbs[i], carry);
     }
 
-    (a, carry)
+    (res, carry)
 }
 
 #[cfg(all(test, feature = "rand_core"))]
 mod tests {
-    use crate::{BoxedUint, Limb, NonZero, Random, RandomMod, U256};
+    use crate::{BoxedUint, Limb, NonZero, Random, RandomMod};
     use rand_core::SeedableRng;
 
-    macro_rules! test_mul_mod_special {
-        ($size:expr, $test_name:ident) => {
-            #[test]
-            fn $test_name() {
-                let mut rng = chacha20::ChaCha8Rng::seed_from_u64(1);
-                let moduli = [
-                    NonZero::<Limb>::random_from_rng(&mut rng),
-                    NonZero::<Limb>::random_from_rng(&mut rng),
+    #[test]
+    fn mul_mod_special() {
+        let mut rng = chacha20::ChaCha8Rng::seed_from_u64(1);
+        let moduli = [
+            NonZero::<Limb>::random_from_rng(&mut rng),
+            NonZero::<Limb>::random_from_rng(&mut rng),
+        ];
+        let sizes = if cfg!(miri) {
+            &[1, 2, 3][..]
+        } else {
+            &[1, 2, 3, 4, 8, 16][..]
+        };
+
+        for size in sizes.iter().copied() {
+            let bits = size * Limb::BITS;
+
+            for special in &moduli {
+                let zero = BoxedUint::zero_with_precision(bits);
+                let one = BoxedUint::one_with_precision(bits);
+                let p = NonZero::new(zero.wrapping_sub(special.get())).unwrap();
+                let minus_one = p.wrapping_sub(Limb::ONE);
+
+                let base_cases = [
+                    (&zero, &zero, &zero),
+                    (&one, &zero, &zero),
+                    (&zero, &one, &zero),
+                    (&one, &one, &one),
+                    (&minus_one, &minus_one, &one),
+                    (&minus_one, &one, &minus_one),
+                    (&one, &minus_one, &minus_one),
                 ];
+                for (a, b, c) in base_cases {
+                    let x = a.mul_mod_special(b, *special.as_ref());
+                    assert_eq!(&x, c, "{} * {} mod {} = {} != {}", a, b, p, x, c);
+                }
 
-                for special in &moduli {
-                    let p =
-                        &NonZero::new(U256::ZERO.wrapping_sub(&U256::from(special.get()))).unwrap();
+                for _i in 0..100 {
+                    let a = BoxedUint::random_mod_vartime(&mut rng, &p);
+                    let b = BoxedUint::random_mod_vartime(&mut rng, &p);
 
-                    let minus_one = p.wrapping_sub(&U256::ONE);
+                    let c = a.mul_mod_special(&b, *special.as_ref());
+                    assert!(&c < p.as_ref(), "not reduced: {} >= {} ", c, p);
 
-                    let base_cases = [
-                        (U256::ZERO, U256::ZERO, U256::ZERO),
-                        (U256::ONE, U256::ZERO, U256::ZERO),
-                        (U256::ZERO, U256::ONE, U256::ZERO),
-                        (U256::ONE, U256::ONE, U256::ONE),
-                        (minus_one, minus_one, U256::ONE),
-                        (minus_one, U256::ONE, minus_one),
-                        (U256::ONE, minus_one, minus_one),
-                    ];
-                    for (a, b, c) in &base_cases {
-                        let (a, b, c) =
-                            (BoxedUint::from(a), BoxedUint::from(b), BoxedUint::from(c));
-                        let x = a.mul_mod_special(&b, *special.as_ref());
-                        assert_eq!(x, c, "{} * {} mod {} = {} != {}", a, b, p, x, c);
-                    }
-
-                    let p = NonZero::new(BoxedUint::from(p.get())).unwrap();
-                    for _i in 0..100 {
-                        let a = BoxedUint::random_mod_vartime(&mut rng, &p);
-                        let b = BoxedUint::random_mod_vartime(&mut rng, &p);
-
-                        let c = a.mul_mod_special(&b, *special.as_ref());
-                        assert!(&c < p.as_ref(), "not reduced: {} >= {} ", c, p);
-
-                        let expected = {
-                            let prod = a.mul(&b);
-                            prod.rem_vartime(&p)
-                        };
-                        assert_eq!(c, expected, "incorrect result");
-                    }
+                    let expected = {
+                        let prod = a.mul(&b);
+                        prod.rem_vartime(&p)
+                    };
+                    assert_eq!(c, expected, "incorrect result");
                 }
             }
-        };
+        }
     }
-
-    test_mul_mod_special!(1, mul_mod_special_1);
-    test_mul_mod_special!(2, mul_mod_special_2);
-    test_mul_mod_special!(3, mul_mod_special_3);
-    test_mul_mod_special!(4, mul_mod_special_4);
-    test_mul_mod_special!(5, mul_mod_special_5);
-    test_mul_mod_special!(6, mul_mod_special_6);
-    test_mul_mod_special!(7, mul_mod_special_7);
-    test_mul_mod_special!(8, mul_mod_special_8);
-    test_mul_mod_special!(9, mul_mod_special_9);
-    test_mul_mod_special!(10, mul_mod_special_10);
-    test_mul_mod_special!(11, mul_mod_special_11);
-    test_mul_mod_special!(12, mul_mod_special_12);
 }
