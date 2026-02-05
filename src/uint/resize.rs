00034b8b1bd251d22a06f4af8fdb1283220980ba
Andrew Whitehead <cywolf@gmail.com>
2026-02-05 15:24:29 -0700

    add Uint::resize_checked method
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/uint/resize.rs
+++ src/uint/resize.rs
@@ -1,4 +1,7 @@
+//! Support for converting between [`Uint`] instances with different sizes.
+
 use super::Uint;
+use crate::{Choice, CtOption};
 
 impl<const LIMBS: usize> Uint<LIMBS> {
     /// Construct a [`Uint<T>`] from the unsigned integer value,
@@ -7,14 +10,20 @@ impl<const LIMBS: usize> Uint<LIMBS> {
     #[inline(always)]
     #[must_use]
     pub const fn resize<const T: usize>(&self) -> Uint<T> {
-        let mut res = Uint::ZERO;
-        let mut i = 0;
-        let dim = if T < LIMBS { T } else { LIMBS };
-        while i < dim {
-            res.limbs[i] = self.limbs[i];
-            i += 1;
-        }
-        res
+        self.as_uint_ref().to_uint_resize()
+    }
+
+    /// Construct a [`Uint<T>`] from the unsigned integer value, returning a [`CtOption`]
+    /// which is `is_none` if the upper limbs were non-zero.
+    #[inline(always)]
+    #[must_use]
+    pub const fn resize_checked<const T: usize>(&self) -> CtOption<Uint<T>> {
+        let is_some = if T < LIMBS {
+            self.as_uint_ref().trailing(T).is_zero()
+        } else {
+            Choice::TRUE
+        };
+        CtOption::new(self.resize(), is_some)
     }
 }
 
@@ -27,6 +36,11 @@ mod tests {
         let u = U64::from_be_hex("AAAAAAAABBBBBBBB");
         let u2: U128 = u.resize();
         assert_eq!(u2, U128::from_be_hex("0000000000000000AAAAAAAABBBBBBBB"));
+        let u3: Option<U128> = u.resize_checked().into_option();
+        assert_eq!(
+            u3,
+            Some(U128::from_be_hex("0000000000000000AAAAAAAABBBBBBBB"))
+        );
     }
 
     #[test]
@@ -34,5 +48,9 @@ mod tests {
         let u = U128::from_be_hex("AAAAAAAABBBBBBBBCCCCCCCCDDDDDDDD");
         let u2: U64 = u.resize();
         assert_eq!(u2, U64::from_be_hex("CCCCCCCCDDDDDDDD"));
+        let u3: Option<U64> = u.resize_checked().into_option();
+        assert_eq!(u3, None);
+        let u4 = U128::ONE.resize_checked().into_option();
+        assert_eq!(u4, Some(U64::ONE));
     }
 }
