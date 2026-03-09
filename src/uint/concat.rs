b525af28bc7e17f998090a37ca2882ea3c2718c6
Andrew Whitehead <cywolf@gmail.com>
2026-03-09 13:39:18 -0600

    add concat_checked, concat_resize, split_checked, split_resize for Uint
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/uint/concat.rs
+++ src/uint/concat.rs
@@ -8,7 +8,7 @@ impl<const LIMBS: usize> Uint<LIMBS> {
     where
         Self: Concat<LIMBS, Output = Uint<WIDE_LIMBS>>,
     {
-        Uint::concat_mixed(self, hi)
+        self.concat_resize(hi)
     }
 
     /// Concatenate the two values, with `self` as least significant and `hi`
@@ -22,17 +22,55 @@ impl<const LIMBS: usize> Uint<LIMBS> {
     where
         Self: Concat<HI_LIMBS, Output = Uint<WIDE_LIMBS>>,
     {
+        self.concat_resize(hi)
+    }
+
+    /// Concatenate the two values, with `self` as least significant and `hi`
+    /// as the most significant. If `WIDE_LIMBS` is not equal to the sum of
+    /// `LIMBS` and `HI_LIMBS`, then `None` is returned.
+    #[inline(always)]
+    #[must_use]
+    pub const fn concat_checked<const HI_LIMBS: usize, const WIDE_LIMBS: usize>(
+        &self,
+        hi: &Uint<HI_LIMBS>,
+    ) -> Option<Uint<WIDE_LIMBS>> {
+        if LIMBS + HI_LIMBS == WIDE_LIMBS {
+            Some(self.concat_resize(hi))
+        } else {
+            None
+        }
+    }
+
+    /// Concatenate the two values, with `self` as least significant and `hi`
+    /// as the most significant. The resulting wide integer may be truncated or
+    /// extended with zeros depending upon whether its size is less than or greater
+    /// than the sum of `LIMBS` and `HI_LIMBS`.
+    #[inline(always)]
+    #[must_use]
+    pub const fn concat_resize<const HI_LIMBS: usize, const WIDE_LIMBS: usize>(
+        &self,
+        hi: &Uint<HI_LIMBS>,
+    ) -> Uint<WIDE_LIMBS> {
         let mut res = Uint::ZERO;
-        let (res_lo, res_hi) = res.as_mut_uint_ref().split_at_mut(LIMBS);
-        res_lo.copy_from(self.as_uint_ref());
-        res_hi.copy_from(hi.as_uint_ref());
+        let len = if LIMBS + HI_LIMBS <= WIDE_LIMBS {
+            LIMBS + HI_LIMBS
+        } else {
+            WIDE_LIMBS
+        };
+        let lo_len = if LIMBS <= len { LIMBS } else { len };
+        let hi_len = len - lo_len;
+        let (res_lo, res_hi) = res.as_mut_uint_ref().split_at_mut(lo_len);
+        res_lo.copy_from(self.as_uint_ref().leading(lo_len));
+        res_hi
+            .leading_mut(hi_len)
+            .copy_from(hi.as_uint_ref().leading(hi_len));
         res
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{BitOps, U64, U128, U192, Uint};
+    use crate::{BitOps, U64, U128, U192, U256, Uint};
 
     #[test]
     fn concat() {
@@ -41,6 +79,35 @@ mod tests {
         assert_eq!(
             lo.concat(&hi),
             U128::from_be_hex("00112233445566778899aabbccddeeff")
+        );
+    }
+
+    #[test]
+    fn concat_checked() {
+        let hi = U64::from_u64(0x0011223344556677);
+        let lo = U64::from_u64(0x8899aabbccddeeff);
+        let Some(wide) = lo.concat_checked(&hi) else {
+            panic!("invalid concat")
+        };
+        assert_eq!(wide, U128::from_u128(0x00112233445566778899aabbccddeeff));
+
+        assert!(lo.concat_checked::<{ U64::LIMBS }, 10>(&hi).is_none());
+    }
+
+    #[test]
+    fn concat_resize_small() {
+        let hi = U128::from_u64(0x0011223344556677);
+        let lo = U128::MAX;
+        assert_eq!(lo.concat_resize(&hi), U64::MAX);
+    }
+
+    #[test]
+    fn concat_resize_large() {
+        let hi = U64::from_u64(0x0011223344556677);
+        let lo = U64::from_u64(0x8899aabbccddeeff);
+        assert_eq!(
+            lo.concat_resize(&hi),
+            U256::from_u128(0x00112233445566778899aabbccddeeff)
         );
     }
 
