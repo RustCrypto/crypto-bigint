41b0e4f594b8772c244f92b93f3839c64a8b2695
Andrew Whitehead <cywolf@gmail.com>
2026-03-09 14:45:31 -0600

    Add concat and split methods for `Uint` without complicated type bounds (#1223)
    Related to #1095
    
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/uint/split.rs
+++ src/uint/split.rs
@@ -7,8 +7,7 @@ impl<const LIMBS: usize> Uint<LIMBS> {
     where
         Self: SplitEven<Output = Uint<HALF_LIMBS>>,
     {
-        let (src_lo, src_hi) = self.as_uint_ref().split_at(HALF_LIMBS);
-        (src_lo.to_uint_resize(), src_hi.to_uint_resize())
+        self.split_resize()
     }
 
     /// Split this number into low and high components respectively.
@@ -20,20 +19,76 @@ impl<const LIMBS: usize> Uint<LIMBS> {
     where
         Self: Split<LO_LIMBS, Output = Uint<HI_LIMBS>>,
     {
-        let (src_lo, src_hi) = self.as_uint_ref().split_at(LO_LIMBS);
+        self.split_resize()
+    }
+
+    /// Split this integer into low and high components. If `LIMBS` is not equal to
+    /// the sum of `LIMBS` and `HI_LIMBS`, then `None` is returned.
+    #[inline(always)]
+    #[must_use]
+    pub const fn split_checked<const LO_LIMBS: usize, const HI_LIMBS: usize>(
+        &self,
+    ) -> Option<(Uint<LO_LIMBS>, Uint<HI_LIMBS>)> {
+        if LO_LIMBS + HI_LIMBS == LIMBS {
+            Some(self.split_resize())
+        } else {
+            None
+        }
+    }
+
+    /// Split this integer into low and high components. The resulting values may be
+    /// truncated or extended with zeros depending upon whether `LIMBS` is greater or
+    /// less than the sum of `LO_LIMBS` and `HI_LIMBS`.
+    #[inline(always)]
+    #[must_use]
+    pub const fn split_resize<const LO_LIMBS: usize, const HI_LIMBS: usize>(
+        &self,
+    ) -> (Uint<LO_LIMBS>, Uint<HI_LIMBS>) {
+        let lo_len = if LO_LIMBS <= LIMBS { LO_LIMBS } else { LIMBS };
+        let (src_lo, src_hi) = self.as_uint_ref().split_at(lo_len);
         (src_lo.to_uint_resize(), src_hi.to_uint_resize())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{BitOps, U64, U128, U192, Uint};
+    use crate::{BitOps, U64, U128, U192, U256, Uint};
 
     #[test]
     fn split() {
         let (lo, hi) = U128::from_be_hex("00112233445566778899aabbccddeeff").split();
         assert_eq!(lo, U64::from_u64(0x8899aabbccddeeff));
         assert_eq!(hi, U64::from_u64(0x0011223344556677));
+    }
+
+    #[test]
+    fn split_checked() {
+        let Some((lo, hi)) = U128::from_be_hex("00112233445566778899aabbccddeeff").split_checked()
+        else {
+            panic!("invalid split")
+        };
+        assert_eq!(lo, U64::from_u64(0x8899aabbccddeeff));
+        assert_eq!(hi, U64::from_u64(0x0011223344556677));
+
+        assert!(
+            U128::from_be_hex("00112233445566778899aabbccddeeff")
+                .split_checked::<8, 10>()
+                .is_none()
+        );
+    }
+
+    #[test]
+    fn split_resize_small() {
+        let (lo, hi) = U256::MAX.split_resize();
+        assert_eq!(lo, U64::MAX);
+        assert_eq!(hi, U64::MAX);
+    }
+
+    #[test]
+    fn split_resize_large() {
+        let (lo, hi) = U128::from_be_hex("00112233445566778899aabbccddeeff").split_resize();
+        assert_eq!(lo, U64::from_u64(0x8899aabbccddeeff));
+        assert_eq!(hi, U128::from_u64(0x0011223344556677));
     }
 
     #[test]
